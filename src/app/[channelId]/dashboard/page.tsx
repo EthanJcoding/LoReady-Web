91539340ff684ee40ab53c566a445613822562cc
seungmin2ee <tmdals2815@gmail.com>
2024-02-28 02:56:12 +0900

    💄 대시보드 레이아웃 스타일링

--- src/app/[channelId]/dashboard/page.tsx
+++ src/app/[channelId]/dashboard/page.tsx
@@ -1,3 +1,13 @@
 export default function Dashboard() {
-  return <div className='flex-1 flex gap-5'></div>
+  return (
+    <div className='flex-1 flex gap-5'>
+      <div className='basis-1/3 p-5 rounded-lg bg-[#ffd2d6]/50 shadow-lg shadow-gray-200/50'>다가오는 레이드</div>
+      <div className='flex-1 grid grid-cols-2 gap-5'>
+        <div className='p-5 rounded-lg bg-white/50 shadow-lg shadow-gray-200/50'>1</div>
+        <div className='p-5 rounded-lg bg-white/50 shadow-lg shadow-gray-200/50'>2</div>
+        <div className='p-5 rounded-lg bg-white/50 shadow-lg shadow-gray-200/50'>3</div>
+        <div className='p-5 rounded-lg bg-white/50 shadow-lg shadow-gray-200/50'>4</div>
+      </div>
+    </div>
+  )
 }
