117c01b39e98799c6a87c9b7627bdfedff264e7e
seungmin2ee <tmdals2815@gmail.com>
2024-02-28 21:57:26 +0900

    🐛 뷰포트 높이 lvh -> dvh 로 변경

--- src/app/[channelId]/layout.tsx
+++ src/app/[channelId]/layout.tsx
@@ -8,7 +8,7 @@ export default function ChannelLayout({
   children: React.ReactNode
 }>) {
   return (
-    <div className={`h-lvh flex bg-[#f8fafb]`}>
+    <div className={`h-dvh flex bg-[#f8fafb]`}>
       <SideBar />
       <main className='flex-1 flex flex-col gap-5 p-7 pb-5'>
         <ServerName />
