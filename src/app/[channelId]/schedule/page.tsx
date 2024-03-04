94711db58ddc3edf1739c7aa5e4ca443ccbc58b0
Kyusik-Jeong <starplanter93@gmail.com>
2024-03-04 17:31:40 +0900

    ✨ CompactRaidCard 구현
    - 대시보드 다가오는 레이드 section 안에 들어갈 레이드 정보 카드 구현했습니다.

--- src/app/[channelId]/schedule/page.tsx
+++ src/app/[channelId]/schedule/page.tsx
@@ -1,3 +1,9 @@
+import CompactRaidCard from '@/components/dashboard/CompactRaidCard'
+
 export default function Schedule() {
-  return <div className='flex-1 flex gap-5'>레이드 일정</div>
+  return (
+    <div className='flex-1 flex gap-5'>
+      <CompactRaidCard boss='아브렐슈드' date='2024.02.23(금) 22:00' headCount='8/8' leader='v최강준일v' />
+    </div>
+  )
 }
