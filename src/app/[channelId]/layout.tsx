32804bc1b5c4995d10fb19153e6391c1b276eeee
seungmin2ee <tmdals2815@gmail.com>
2024-03-06 00:00:08 +0900

    ✨ 다크모드 셋팅

--- src/app/[channelId]/layout.tsx
+++ src/app/[channelId]/layout.tsx
@@ -1,6 +1,7 @@
 import Navigation from '@/components/layout/Navigation'
 import ServerName from '@/components/layout/ServerName'
 import SideBar from '@/components/layout/sidebar/SideBar'
+import Providers from './Providers'
 
 export default function ChannelLayout({
   children
@@ -8,13 +9,15 @@ export default function ChannelLayout({
   children: React.ReactNode
 }>) {
   return (
-    <div className={`h-dvh flex bg-[#f8fafb]`}>
-      <SideBar />
-      <main className='flex-1 flex flex-col gap-5 p-7 pb-5'>
-        <ServerName />
-        <Navigation />
-        {children}
-      </main>
-    </div>
+    <Providers>
+      <div className='h-dvh flex bg-[#f8fafb]'>
+        <SideBar />
+        <main className='flex-1 flex flex-col gap-5 p-7 pb-5'>
+          <ServerName />
+          <Navigation />
+          {children}
+        </main>
+      </div>
+    </Providers>
   )
 }
