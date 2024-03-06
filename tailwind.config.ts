6199270e5bf42f69c1efcd49620a7303d31546ac
seungmin2ee <tmdals2815@gmail.com>
2024-03-06 20:36:14 +0900

    🔧 tailwind.config에 color 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,16 @@ const config: Config = {
       },
       fontFamily: {
         pretendard: ['var(--font-pretendard)']
+      },
+      colors: {
+        light: '#f8fafb',
+        dark: '#272727',
+        'primary-gray': '#9b9b9b',
+        'secondary-gray': '#dde1e6',
+        'primary-blue': '#c7e1ee',
+        'secondary-blue': '#edfcff',
+        'primary-accent': '#00a4e8',
+        'secondary-accent': '#ffd2d6'
       }
     }
   },
