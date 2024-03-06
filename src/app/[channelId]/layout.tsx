4c04c72ebdf8f3f04031c7588ca11a8823cd9a05
seungmin2ee <tmdals2815@gmail.com>
2024-03-06 21:31:45 +0900

    💄 사이드바 다크모드 스타일 추가

--- src/app/[channelId]/layout.tsx
+++ src/app/[channelId]/layout.tsx
@@ -10,7 +10,7 @@ export default function ChannelLayout({
 }>) {
   return (
     <Providers>
-      <div className='h-dvh flex bg-[#f8fafb]'>
+      <div className='h-dvh flex bg-light dark:bg-dark dark:text-light'>
         <SideBar />
         <main className='flex-1 flex flex-col gap-5 p-7 pb-5'>
           <ServerName />
