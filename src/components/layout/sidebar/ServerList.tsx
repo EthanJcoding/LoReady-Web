d633417a753333660439e57ee6f721c704acb821
seungmin2ee <tmdals2815@gmail.com>
2024-03-08 03:04:09 +0900

    💄 사이드바-서버 리스트 스타일링

---
+++ src/components/layout/sidebar/ServerList.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function ServerList() {
+  return (
+    <li className='flex opacity-85 hover:opacity-100 transition-opacity'>
+      <Link href={'/1/dashboard'} className='flex items-center gap-2'>
+        <span className='relative block w-6 h-6 rounded-full border overflow-hidden'>
+          <Image
+            src={'https://cdn.discordapp.com/icons/1209059689657016371/0f0a953c0d6c76e50b88304b1b00032e'}
+            alt='서버 아이콘'
+            fill
+            sizes='100px'
+            style={{
+              objectFit: 'cover'
+            }}
+            placeholder='blur'
+            blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mN8+vx1PQAIqAM4jZDFJQAAAABJRU5ErkJggg==s'
+          ></Image>
+        </span>
+        <span className='max-w-48 truncate'>올바른취업생활</span>
+      </Link>
+    </li>
+  )
+}
