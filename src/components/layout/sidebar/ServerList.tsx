8c64c5f2bab9e02ace0f39d7daa7354840fc5959
seungmin2ee <tmdals2815@gmail.com>
2024-03-08 23:28:42 +0900

    💄 Image 컴포넌트 스타일관련 속성 수정

--- src/components/layout/sidebar/ServerList.tsx
+++ src/components/layout/sidebar/ServerList.tsx
@@ -5,15 +5,13 @@ export default function ServerList() {
   return (
     <li className='flex opacity-85 hover:opacity-100 transition-opacity'>
       <Link href={'/1/dashboard'} className='flex items-center gap-2'>
-        <span className='relative block w-6 h-6 rounded-full border overflow-hidden'>
+        <span className='w-6 h-6 rounded-full border overflow-hidden'>
           <Image
+            className='w-full h-full object-cover'
             src={'https://cdn.discordapp.com/icons/1209059689657016371/0f0a953c0d6c76e50b88304b1b00032e'}
             alt='서버 아이콘'
-            fill
-            sizes='100px'
-            style={{
-              objectFit: 'cover'
-            }}
+            width={100}
+            height={100}
             placeholder='blur'
             blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mN8+vx1PQAIqAM4jZDFJQAAAABJRU5ErkJggg==s'
           ></Image>
