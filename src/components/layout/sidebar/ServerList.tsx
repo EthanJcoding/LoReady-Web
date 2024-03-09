4c57bb9a29105faa35aebcfcafd6be8e71086fae
seungmin2ee <tmdals2815@gmail.com>
2024-03-10 03:07:39 +0900

    ✨ 서버리스트 active 효과 추가

--- src/components/layout/sidebar/ServerList.tsx
+++ src/components/layout/sidebar/ServerList.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import ServerProfile from '@/components/ServerProfile'
 import Link from 'next/link'
+import { useParams } from 'next/navigation'
 
 interface Ownprops {
   id: string
@@ -8,9 +11,12 @@ interface Ownprops {
 }
 
 export default function ServerList({ id, name, iconSrc }: Ownprops) {
+  const { channelId } = useParams()
+  const active = channelId === id
+
   return (
-    <li className='flex opacity-85 hover:opacity-100 transition-opacity'>
-      <Link href={`/${id}/dashboard`} className='overflow-hidden'>
+    <li className={`flex ${active ? 'opacity-100' : 'opacity-80'} hover:opacity-100 transition-opacity`}>
+      <Link href={`/${id}/dashboard`} className={`overflow-hidden ${active && 'font-medium text-primary-accent'}`}>
         <ServerProfile name={name} iconSrc={iconSrc} iconSize={6} />
       </Link>
     </li>
