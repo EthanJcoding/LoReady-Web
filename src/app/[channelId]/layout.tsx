b0931a4b01b28794d1306d9795c0685ce09bf5f7
seungmin2ee <tmdals2815@gmail.com>
2024-03-10 00:17:54 +0900

    ✨ 서버명 데이터 표출

--- src/app/[channelId]/layout.tsx
+++ src/app/[channelId]/layout.tsx
@@ -3,17 +3,20 @@ import ServerName from '@/components/layout/ServerName'
 import SideBar from '@/components/layout/sidebar/SideBar'
 import Providers from './Providers'
 
-export default function ChannelLayout({
-  children
-}: Readonly<{
+interface Ownprops {
   children: React.ReactNode
-}>) {
+  params: {
+    channelId: string
+  }
+}
+
+export default function ChannelLayout({ children, params: { channelId } }: Ownprops) {
   return (
     <Providers>
       <div className='h-dvh flex bg-light dark:bg-dark dark:text-light'>
         <SideBar />
         <main className='flex-1 flex flex-col gap-5 p-7 pb-5 overflow-hidden'>
-          <ServerName />
+          <ServerName id={channelId} />
           <Navigation />
           {children}
         </main>
--- src/components/layout/ServerName.tsx
+++ src/components/layout/ServerName.tsx
@@ -1,22 +1,31 @@
+import { getChannelData } from '@/api/firebase'
 import Image from 'next/image'
 
-export default function ServerName() {
+interface Ownprops {
+  id: string
+}
+
+export default async function ServerName({ id }: Ownprops) {
+  //FIXME: 접속 url 확인되면 아래 id값 재할당 삭제
+  id = '1050686760373469234'
+  const data = await getChannelData(id)
+
+  if (!data) return
+
   return (
     <div className='flex items-center gap-2 pb-4 text-2xl'>
       <span className='flex-none w-9 h-9 rounded-full border overflow-hidden'>
         <Image
           className='w-full h-full object-cover'
-          src={'https://cdn.discordapp.com/icons/1209059689657016371/0f0a953c0d6c76e50b88304b1b00032e'}
-          alt='서버 아이콘'
+          src={data.channelIconURL}
+          alt={`${data.channelName} 서버 아이콘`}
           width={100}
           height={100}
           placeholder='blur'
           blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mN8+vx1PQAIqAM4jZDFJQAAAABJRU5ErkJggg==s'
         ></Image>
       </span>
-      <span className='truncate'>
-        올바른취업생활올바른취업생활올바른취업생활올바른취업생활올바른취업생활올바른취업생활올바른취업생활
-      </span>
+      <span className='truncate'>{data.channelName}</span>
     </div>
   )
 }
