a3b61d9931cb657e2107327e11e35fbba075da1c
seungmin2ee <tmdals2815@gmail.com>
2024-03-09 23:25:45 +0900

    💄 사이드바 스타일 수정

--- src/components/layout/sidebar/ServerList.tsx
+++ src/components/layout/sidebar/ServerList.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link'
 export default function ServerList() {
   return (
     <li className='flex opacity-85 hover:opacity-100 transition-opacity'>
-      <Link href={'/1/dashboard'} className='flex items-center gap-2'>
-        <span className='w-6 h-6 rounded-full border overflow-hidden'>
+      <Link href={'/1/dashboard'} className='flex items-center gap-2 overflow-hidden'>
+        <span className='flex-none w-6 h-6 rounded-full border overflow-hidden'>
           <Image
             className='w-full h-full object-cover'
             src={'https://cdn.discordapp.com/icons/1209059689657016371/0f0a953c0d6c76e50b88304b1b00032e'}
@@ -16,7 +16,7 @@ export default function ServerList() {
             blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mN8+vx1PQAIqAM4jZDFJQAAAABJRU5ErkJggg==s'
           ></Image>
         </span>
-        <span className='max-w-48 truncate'>올바른취업생활</span>
+        <span className='truncate'>올바른취업생활올바른취업생활올바른취업생활</span>
       </Link>
     </li>
   )
