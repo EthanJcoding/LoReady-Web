e736266dbcd657b3ac6e23033df0a18969b39b68
seungmin2ee <tmdals2815@gmail.com>
2024-03-09 23:11:40 +0900

    💄 main layout에 overflow 속성 추가

--- src/app/[channelId]/layout.tsx
+++ src/app/[channelId]/layout.tsx
@@ -12,7 +12,7 @@ export default function ChannelLayout({
     <Providers>
       <div className='h-dvh flex bg-light dark:bg-dark dark:text-light'>
         <SideBar />
-        <main className='flex-1 flex flex-col gap-5 p-7 pb-5'>
+        <main className='flex-1 flex flex-col gap-5 p-7 pb-5 overflow-hidden'>
           <ServerName />
           <Navigation />
           {children}
