f1ae284810364f2eb8be3b2e1c5a21981eb408d5
seungmin2ee <tmdals2815@gmail.com>
2024-03-10 02:23:24 +0900

    ✨ 서버아이콘+서버명 재사용 컴포넌트화

--- src/components/layout/ServerName.tsx
+++ src/components/layout/ServerName.tsx
@@ -1,5 +1,5 @@
 import { getChannelData } from '@/api/firebase'
-import Image from 'next/image'
+import ServerProfile from '../ServerProfile'
 
 interface Ownprops {
   id: string
@@ -13,19 +13,8 @@ export default async function ServerName({ id }: Ownprops) {
   if (!data) return
 
   return (
-    <div className='flex items-center gap-2 pb-4 text-2xl'>
-      <span className='flex-none w-9 h-9 rounded-full border overflow-hidden'>
-        <Image
-          className='w-full h-full object-cover'
-          src={data.channelIconURL}
-          alt={`${data.channelName} 서버 아이콘`}
-          width={100}
-          height={100}
-          placeholder='blur'
-          blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mN8+vx1PQAIqAM4jZDFJQAAAABJRU5ErkJggg==s'
-        ></Image>
-      </span>
-      <span className='truncate'>{data.channelName}</span>
+    <div className='pb-4'>
+      <ServerProfile name={data.channelName} iconSrc={data.channelIconURL} iconSize={9} fontSize='2xl' />
     </div>
   )
 }
