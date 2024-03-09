b9f0b17231c93f2fb868a098c7a106a3db903274
seungmin2ee <tmdals2815@gmail.com>
2024-03-10 05:04:17 +0900

    🐛 ServerProfile 컴포넌트의 유틸리티클래스 적용되지 않는 이슈

--- src/components/layout/sidebar/ServerList.tsx
+++ src/components/layout/sidebar/ServerList.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import ServerProfile from '@/components/ServerProfile'
+import Image from 'next/image'
 import Link from 'next/link'
 import { useParams } from 'next/navigation'
 
@@ -16,8 +16,22 @@ export default function ServerList({ id, name, iconSrc }: Ownprops) {
 
   return (
     <li className={`flex ${active ? 'opacity-100' : 'opacity-80'} hover:opacity-100 transition-opacity`}>
-      <Link href={`/${id}/dashboard`} className={`overflow-hidden ${active && 'font-medium text-primary-accent'}`}>
-        <ServerProfile name={name} iconSrc={iconSrc} iconSize={6} />
+      <Link
+        href={`/${id}/dashboard`}
+        className={`flex items-center gap-2 overflow-hidden ${active && 'font-medium text-primary-accent'}`}
+      >
+        <span className='flex-none w-6 h-6 rounded-full border overflow-hidden'>
+          <Image
+            className='w-full h-full object-cover'
+            src={iconSrc}
+            alt={`${name} 서버 아이콘`}
+            width={100}
+            height={100}
+            placeholder='blur'
+            blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mN8+vx1PQAIqAM4jZDFJQAAAABJRU5ErkJggg==s'
+          />
+        </span>
+        <span className='truncate'>{name}</span>
       </Link>
     </li>
   )
