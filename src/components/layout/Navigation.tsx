3e56525bfcb24d0cbf66b12a4374c74cb54eb046
kongchip <myunghwan0321@gmail.com>
2024-03-11 23:05:38 +0900

    🐛 새로고침이 nav초기화및 router초기화 되지않은 불일치 버그 수정

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,17 +1,10 @@
 'use client'
 import Link from 'next/link'
-import { usePathname, useRouter } from 'next/navigation'
+import { useParams, usePathname } from 'next/navigation'
 
-interface OwnProps {
-  channelId: string
-}
-export default function Navigation({ channelId }: OwnProps) {
-  const router = useRouter()
-
+export default function Navigation() {
+  const { channelId } = useParams()
   const pathname = usePathname()
-  const handleClick = (event: string) => {
-    router.push(`/${channelId}/${event}`)
-  }
 
   return (
     <nav className='border-b border-secondary-gray/50 dark:border-primary-gray/50'>
@@ -19,14 +12,10 @@ export default function Navigation({ channelId }: OwnProps) {
         <li
           className={`px-4 py-1 ${pathname === `/${channelId}/dashboard` ? 'border-b-2 border-black font-bold' : ''}`}
         >
-          <Link href={`/${channelId}/dashboard`} onClick={() => handleClick('dashboard')}>
-            대시보드
-          </Link>
+          <Link href={`/${channelId}/dashboard`}>대시보드</Link>
         </li>
         <li className={`px-4 py-1 ${pathname === `/${channelId}/schedule` ? 'border-b-2 border-black font-bold' : ''}`}>
-          <Link href={`/${channelId}/schedule`} onClick={() => handleClick('schedule')}>
-            레이드 일정
-          </Link>
+          <Link href={`/${channelId}/schedule`}>레이드 일정</Link>
         </li>
       </ul>
     </nav>
