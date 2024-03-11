525dd4c6a4d8498f3f1f412e8c9ca6dd160c7f57
kongchip <myunghwan0321@gmail.com>
2024-03-11 21:59:58 +0900

    🔥 불필요한 코드 삭제

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,7 +1,5 @@
 'use client'
 import Link from 'next/link'
-import { useState } from 'react'
-// import { useRouter } from 'next/router'
 import { usePathname, useRouter } from 'next/navigation'
 
 interface OwnProps {
