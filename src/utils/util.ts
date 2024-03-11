6373f441e54a7f419bcec728f30146baa752e78e
seungmin2ee <tmdals2815@gmail.com>
2024-03-12 03:07:24 +0900

    ✨ 보스명과 난이도, 레이드 정원 추출 함수 추가

---
+++ src/utils/util.ts
@@ -0,0 +1,25 @@
+export const extractBossRank = (raidName: string) => {
+  let [boss, rank] = raidName.split(' ')
+
+  rank = rank.substring(1, rank.length - 1)
+
+  switch (rank) {
+    case '노말':
+      rank = 'NOMAL'
+      break
+    case '하드':
+      rank = 'HARD'
+      break
+    case '헬':
+      rank = 'HELL'
+      break
+  }
+
+  return { boss, rank }
+}
+
+export const extractCapacity = (raidType: string) => {
+  const capacity = raidType.substring(0, raidType.indexOf('인'))
+
+  return capacity
+}
