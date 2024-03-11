d1fbef23aa3cd21a32edb22e28852b2c4bfd1bb2
Kyusik-Jeong <starplanter93@gmail.com>
2024-03-11 17:54:31 +0900

    ✨ 1파티 2파티 추가

--- src/stores/raiders.ts
+++ src/stores/raiders.ts
@@ -9,10 +9,18 @@ interface RaidersType {
 
 interface RaidersStoreType {
   raiders: RaidersType[]
+  firstPartyRaiders: RaidersType[]
+  secondPartyRaiders: RaidersType[]
   setRaiders: (raiders: RaidersType[]) => void
+  setFirstPartyRaiders: (raiders: RaidersType[]) => void
+  setSecondPartyRaiders: (raiders: RaidersType[]) => void
 }
 
 export const useRaidersStore = create<RaidersStoreType>(set => ({
   raiders: [],
-  setRaiders: raiders => set({ raiders })
+  firstPartyRaiders: [],
+  secondPartyRaiders: [],
+  setRaiders: raiders => set({ raiders }),
+  setFirstPartyRaiders: raiders => set({ raiders }),
+  setSecondPartyRaiders: raiders => set({ raiders })
 }))
