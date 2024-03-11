219a1ef1e5c797b2f93e2f1469c2b2802c672318
seungmin2ee <tmdals2815@gmail.com>
2024-03-12 01:21:15 +0900

    ✨ 다가오는 레이드 데이터 표출

--- src/components/dashboard/UpcomingRaid.tsx
+++ src/components/dashboard/UpcomingRaid.tsx
@@ -1,11 +1,22 @@
+import { getChannelData } from '@/api/firebase'
 import RaidList from './RaidList'
+interface Ownprops {
+  channelId: string
+}
 
-export default function UpcomingRaid() {
+export default async function UpcomingRaid({ channelId }: Ownprops) {
+  //FIXME: 작업 완료 후 channelId 할당 삭제하기
+  channelId = '1050686760373469234'
+  const data = await getChannelData(channelId)
+
+  if (!data) return
   return (
     <div className='card basis-1/3 bg-secondary-accent/50 dark:bg-primary-accent/80'>
       <div className='pb-4 text-xl'>다가오는 레이드</div>
       <ul className='flex flex-col gap-3'>
-        <RaidList />
+        {data.schedules.map((scheduleId: string) => (
+          <RaidList key={scheduleId} scheduleId={scheduleId} />
+        ))}
       </ul>
     </div>
   )
