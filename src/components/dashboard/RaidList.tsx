b58abad134bde43924d3db921a6ab96fcbf9b0d1
seungmin2ee <tmdals2815@gmail.com>
2024-03-12 20:24:12 +0900

    ✨  레이드 데이터 가공해서 내려주도록 수정

--- src/components/dashboard/RaidList.tsx
+++ src/components/dashboard/RaidList.tsx
@@ -1,6 +1,7 @@
 import { getScheduleData } from '@/api/firebase'
 import CompactRaidCard from './CompactRaidCard'
 import Link from 'next/link'
+import { extractBossRank, extractCapacity } from '@/utils/util'
 
 interface Ownprops {
   scheduleId: string
@@ -10,14 +11,20 @@ export default async function RaidList({ scheduleId }: Ownprops) {
   const data = await getScheduleData(scheduleId)
 
   if (!data) return
+
+  const { boss, rank } = extractBossRank(data.raidName)
+  const capacity = extractCapacity(data.raidType)
+  const participants = data.participants.length
+
   return (
     <li>
       <Link href={`/${data.channel}/schedule/${scheduleId}`}>
         <CompactRaidCard
-          boss={data.raidName}
+          boss={boss}
+          rank={rank}
           leader={data.raidLeader.character}
           date={data.raidDate}
-          headCount={data.raidType}
+          headCount={`${participants} / ${capacity}`}
         />
       </Link>
     </li>
