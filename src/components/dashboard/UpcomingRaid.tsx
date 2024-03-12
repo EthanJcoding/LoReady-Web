23811890ccaa9461cc2a1725f60b0a4d2cc5aa6b
seungmin2ee <tmdals2815@gmail.com>
2024-03-12 21:13:06 +0900

    💄 UpcomingRaid 스타일 수정

--- src/components/dashboard/UpcomingRaid.tsx
+++ src/components/dashboard/UpcomingRaid.tsx
@@ -6,12 +6,12 @@ interface Ownprops {
 
 export default async function UpcomingRaid({ channelId }: Ownprops) {
   //FIXME: 작업 완료 후 channelId 할당 삭제하기
-  channelId = '1050686760373469234'
+  channelId = '1209059689657016371'
   const data = await getChannelData(channelId)
 
   if (!data) return
   return (
-    <div className='card basis-1/3 bg-secondary-accent/50 dark:bg-primary-accent/80'>
+    <div className='card min-w-[340px] basis-1/3 bg-secondary-accent/50 dark:bg-primary-accent/80'>
       <div className='pb-4 text-xl'>다가오는 레이드</div>
       <ul className='flex flex-col gap-3'>
         {data.schedules.map((scheduleId: string) => (
