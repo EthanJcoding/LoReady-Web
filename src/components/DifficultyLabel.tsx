9b5790eda1221c55c59ea1c3ac960b16a5289276
seungmin2ee <tmdals2815@gmail.com>
2024-03-12 21:54:40 +0900

    ✨ 난이도에 따른 DifficultyLable 컬러 설정 함수 추가

--- src/components/DifficultyLabel.tsx
+++ src/components/DifficultyLabel.tsx
@@ -1,15 +1,16 @@
 interface Ownprops {
   label: string
-  color?: Color
+  color?: Color | null
 }
 
 type Color = 'red' | 'yellow' | 'black' | 'blue' | 'green'
 
-export default function DifficultyLabel({ label, color = 'red' }: Ownprops) {
+export default function DifficultyLabel({ label, color }: Ownprops) {
   return (
     <div
-      className={`flex items-center min-w-5 h-5 px-2 py-1 ${changeBgColor(color)}
-       text-white text-xs text-center align-middle rounded-full`}
+      className={`flex items-center min-w-5 h-5 px-2 py-1 ${
+        color ? changeBgColor(color) : setLableColor(label)
+      } text-white text-xs text-center align-middle rounded-full`}
     >
       {label}
     </div>
@@ -28,5 +29,16 @@ function changeBgColor(color: Color) {
       return 'bg-sky-500'
     case 'green':
       return 'bg-green-500'
+  }
+}
+
+function setLableColor(label: string) {
+  switch (label) {
+    case 'NORMAL':
+      return 'bg-neutral-950'
+    case 'HARD':
+      return 'bg-amber-400'
+    case 'HELL':
+      return 'bg-red-600'
   }
 }
