b399f44c0820887f855a8882298eeb9950d7dcfa
kongchip <myunghwan0321@gmail.com>
2024-03-14 01:05:35 +0900

    ✨ PartyListDetail 컴포넌트 Link 기능 추가

--- src/components/partyList/PartyListDetail.tsx
+++ src/components/partyList/PartyListDetail.tsx
@@ -1,4 +1,5 @@
 import { getScheduleData } from '@/api/firebase/getScheduleData/getScheduleData'
+import Link from 'next/link'
 
 interface Ownprops {
   scheduleId: string
@@ -24,28 +25,32 @@ export default async function PartyListDetail({ scheduleId }: Ownprops) {
 
   const data = await getScheduleData(scheduleId)
 
+  if (!data) return
+
   return (
-    <div className='grid grid-cols-2 gap-4 pt-2 px-3 text-center'>
-      <div>
-        <div className='bg-lime-400 rounded-md font-bold text-white '>1번파티</div>
-        <ul className='gap-1'>
-          {data?.characters.party0.map((party: ScheduleData, index: number) => (
-            <li className='bg-slate-300 mt-1 rounded-md' key={index}>
-              {party.character}
-            </li>
-          ))}
-        </ul>
+    <Link href={`/${data.channel}/schedule/${scheduleId}`}>
+      <div className='grid grid-cols-2 gap-4 pt-2 px-3 text-center'>
+        <div>
+          <div className='bg-lime-400 rounded-md font-bold text-white '>1번파티</div>
+          <ul className='gap-1'>
+            {data?.characters.party0.map((party: ScheduleData, index: number) => (
+              <li className='bg-slate-300 mt-1 rounded-md' key={index}>
+                {party.character}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <div className='bg-violet-600 rounded-md font-bold text-white'>2번파티</div>
+          <ul className='gap-1'>
+            {data?.characters.party0.map((party: ScheduleData, index: number) => (
+              <li className='bg-slate-300 mt-1 rounded-md' key={index}>
+                {party.character}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div>
-        <div className='bg-violet-600 rounded-md font-bold text-white'>2번파티</div>
-        <ul className='gap-1'>
-          {data?.characters.party0.map((party: ScheduleData, index: number) => (
-            <li className='bg-slate-300 mt-1 rounded-md' key={index}>
-              {party.character}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    </Link>
   )
 }
