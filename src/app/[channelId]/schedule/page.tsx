94669dec7f2e344b635325f93ed9035660b9a037
kongchip <myunghwan0321@gmail.com>
2024-03-13 21:32:57 +0900

    💄 스크롤 기능 추가

--- src/app/[channelId]/schedule/page.tsx
+++ src/app/[channelId]/schedule/page.tsx
@@ -3,7 +3,7 @@ import PartyList from '@/components/partyList/PartyList'
 
 export default function Schedule() {
   return (
-    <div className='flex-1 flex gap-5 flex-wrap'>
+    <div className='flex-1 flex gap-5 flex-wrap overflow-y-scroll overscroll-y-auto'>
       <div className='border-solid border-2 h-[260px] rounded-md'>
         <CompactRaidCard boss='아브렐슈드' date='2024.02.23(금) 22:00' headCount='8/8' leader='v최강준일v' />
         <PartyList />
