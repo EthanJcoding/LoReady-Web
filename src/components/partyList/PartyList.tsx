165973ff5b848858ff52df3dc2cd0accb978a87a
kongchip <myunghwan0321@gmail.com>
2024-03-14 01:30:14 +0900

    🔥 불필요한 코드 삭제 및 PartyListDetail 위치 수정

--- src/components/partyList/PartyList.tsx
+++ src/components/partyList/PartyList.tsx
@@ -1,4 +1,4 @@
-import { getChannelData, getScheduleData } from '@/api/firebase'
+import { getChannelData } from '@/api/firebase'
 import PartyListDetail from './PartyListDetail'
 import RaidList from '@/components/dashboard/RaidList'
 
@@ -16,9 +16,11 @@ export default async function PartyList({ channelId }: Ownprops) {
     <div className='h-auto border-solid border-2 rounded-md w-full md:w-[350px]'>
       <ul className='flex flex-col gap-3 pb-2.5'>
         {data.schedules.map((scheduleId: string) => (
-          <RaidList key={scheduleId} scheduleId={scheduleId} />
+          <>
+            <RaidList key={scheduleId} scheduleId={scheduleId} />
+            <PartyListDetail scheduleId={data.scheduleId} />
+          </>
         ))}
-        <PartyListDetail scheduleId={data.scheduleId} />
       </ul>
     </div>
   )
