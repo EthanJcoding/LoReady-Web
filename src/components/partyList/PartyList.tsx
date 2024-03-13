b22353e922f7d2e4e79f8dc02129220140b8100f
kongchip <myunghwan0321@gmail.com>
2024-03-14 00:43:52 +0900

    🔥 channelId에 따라 파티 리스트 데이터를 가져올 수 있도록 수정

--- src/components/partyList/PartyList.tsx
+++ src/components/partyList/PartyList.tsx
@@ -1,58 +1,25 @@
-'use client'
-import { getScheduleData } from '@/api/firebase/getScheduleData/getScheduleData'
-import { useEffect, useState } from 'react'
+import { getChannelData } from '@/api/firebase'
+import PartyListDetail from './PartyListDetail'
+import RaidList from '@/components/dashboard/RaidList'
 
-interface ScheduleData {
-  characters: {
-    party0: {
-      character: string
-    }[]
-    party1: { name: string }[]
-    party2: { name: string }[]
-  }
-  raidDate: string
-  raidLeader: {
-    character: string
-  }
-  raidName: string
-  channel: string
+interface Ownprops {
+  channelId: string
 }
 
-export default function PartyList() {
-  const [scheduleData, setScheduleData] = useState<ScheduleData | null>(null)
-  useEffect(() => {
-    getScheduleData('57FNJZW3G0hI2hXcVK1v')
-      .then(data => {
-        const scheduleData: ScheduleData = data as ScheduleData
-        setScheduleData(scheduleData)
-      })
-      .catch(error => console.error(error))
-  }, [])
+export default async function PartyList({ channelId }: Ownprops) {
+  channelId = '1209059689657016371'
+  const data = await getChannelData(channelId)
 
-  // 넘겨줘야할 데이터 : 각 파티 정보(디스코드 아이디, 캐릭터닉네임, 레벨, 클래스),title, createdAt, startTime, raidType
+  if (!data) return
 
   return (
-    <div className='grid grid-cols-2 gap-4 pt-2 px-3 text-center'>
-      <div>
-        <div className='bg-lime-400 rounded-md font-bold text-white '>1번파티</div>
-        <ul className='gap-1'>
-          {scheduleData?.characters.party0.map((party, index) => (
-            <li className='bg-slate-300 mt-1 rounded-md' key={index}>
-              {party.character}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div>
-        <div className='bg-violet-600 rounded-md font-bold text-white'>2번파티</div>
-        <ul className='gap-1'>
-          {scheduleData?.characters.party0.map((party, index) => (
-            <li className='bg-slate-300 mt-1 rounded-md' key={index}>
-              {party.character}
-            </li>
-          ))}
-        </ul>
-      </div>
+    <div className='h-auto border-solid border-2 rounded-md w-full md:w-[350px]'>
+      <ul className='flex flex-col gap-3 pb-2.5'>
+        {data.schedules.map((scheduleId: string) => (
+          <RaidList key={scheduleId} scheduleId={scheduleId} />
+        ))}
+        <PartyListDetail scheduleId={data.scheduleId} />
+      </ul>
     </div>
   )
 }
