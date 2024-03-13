a926b485128c2c39f4a23e8e8c82c1107f8fdb0d
kongchip <myunghwan0321@gmail.com>
2024-03-13 21:34:55 +0900

    🔥 store데이터 코드 삭제 및 서버데이터로 리펙토링

--- src/components/partyList/PartyList.tsx
+++ src/components/partyList/PartyList.tsx
@@ -1,37 +1,44 @@
 'use client'
-// 파이어베이스에서 데이터 가져오기 : zustand로 가져오기?
+import { getScheduleData } from '@/api/firebase/getScheduleData/getScheduleData'
+import { useEffect, useState } from 'react'
 
-import { create } from 'zustand'
-type Store = {
-  party1: {
-    name: string
-  }[]
-  party2: {
-    name: string
-  }[]
+interface ScheduleData {
+  characters: {
+    party0: {
+      character: string
+    }[]
+    party1: { name: string }[]
+    party2: { name: string }[]
+  }
+  raidDate: string
+  raidLeader: {
+    character: string
+  }
+  raidName: string
+  channel: string
 }
 
-const useStore = create<Store>(() => ({
-  party1: [
-    { name: '파이어베이스장인' },
-    { name: '쉽지않은파이어베이스' },
-    { name: '할수있다파이어베이스' },
-    { name: '킹갓준일님' }
-  ],
-  party2: [{ name: 'v최강준일v' }, { name: '달래비빔밥' }, { name: '어묵탕탕수육' }, { name: '로레디짱' }]
-}))
-
 export default function PartyList() {
-  const { party1, party2 } = useStore()
+  const [scheduleData, setScheduleData] = useState<ScheduleData | null>(null)
+  useEffect(() => {
+    getScheduleData('57FNJZW3G0hI2hXcVK1v')
+      .then(data => {
+        const scheduleData: ScheduleData = data as ScheduleData
+        setScheduleData(scheduleData)
+      })
+      .catch(error => console.error(error))
+  }, [])
+
+  // 넘겨줘야할 데이터 : 각 파티 정보(디스코드 아이디, 캐릭터닉네임, 레벨, 클래스),title, createdAt, startTime, raidType
 
   return (
     <div className='grid grid-cols-2 gap-4 pt-2 px-3 text-center'>
       <div>
         <div className='bg-lime-400 rounded-md font-bold text-white '>1번파티</div>
         <ul className='gap-1'>
-          {party1.map((party, index) => (
+          {scheduleData?.characters.party0.map((party, index) => (
             <li className='bg-slate-300 mt-1 rounded-md' key={index}>
-              {party.name}
+              {party.character}
             </li>
           ))}
         </ul>
@@ -39,9 +46,9 @@ export default function PartyList() {
       <div>
         <div className='bg-violet-600 rounded-md font-bold text-white'>2번파티</div>
         <ul className='gap-1'>
-          {party2.map((party, index) => (
+          {scheduleData?.characters.party0.map((party, index) => (
             <li className='bg-slate-300 mt-1 rounded-md' key={index}>
-              {party.name}
+              {party.character}
             </li>
           ))}
         </ul>
