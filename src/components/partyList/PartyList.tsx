92317e8b4ed142a3d8b089b9eac2568b5a35c72b
kongchip <myunghwan0321@gmail.com>
2024-03-14 22:11:24 +0900

    ✨ RaidList 컴포넌트 플래그 생성

--- src/components/partyList/PartyList.tsx
+++ src/components/partyList/PartyList.tsx
@@ -1,6 +1,7 @@
 import { getChannelData } from '@/api/firebase'
 import PartyListDetail from './PartyListDetail'
 import RaidList from '@/components/dashboard/RaidList'
+import Link from 'next/link'
 
 interface Ownprops {
   channelId: string
@@ -9,17 +10,17 @@ interface Ownprops {
 export default async function PartyList({ channelId }: Ownprops) {
   channelId = '1209059689657016371'
   const data = await getChannelData(channelId)
+  const enableRouting = false
 
   if (!data) return
-
   return (
     <div className='h-auto border-solid border-2 rounded-md md:w-[350px]'>
       <ul className='flex flex-col gap-3 pb-2.5'>
         {data.schedules.map((scheduleId: string) => (
-          <>
-            <RaidList key={scheduleId} scheduleId={scheduleId} />
+          <Link href={`/${channelId}/schedule/${scheduleId}`}>
+            <RaidList key={scheduleId} scheduleId={scheduleId} enableRouting={enableRouting} />
             <PartyListDetail scheduleId={data.scheduleId} />
-          </>
+          </Link>
         ))}
       </ul>
     </div>
