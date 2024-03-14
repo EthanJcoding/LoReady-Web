0f82b67e76c5f3c60f235b5d07e809a7175db00d
kongchip <myunghwan0321@gmail.com>
2024-03-14 16:30:59 +0900

    🔥 옵셔널 코드 삭제

--- src/components/partyList/PartyListDetail.tsx
+++ src/components/partyList/PartyListDetail.tsx
@@ -33,7 +33,7 @@ export default async function PartyListDetail({ scheduleId }: Ownprops) {
         <div>
           <div className='bg-lime-400 rounded-md font-bold text-white '>1번파티</div>
           <ul className='gap-1'>
-            {data?.characters.party0.map((party: ScheduleData, index: number) => (
+            {data.characters.party0.map((party: ScheduleData, index: number) => (
               <li className='bg-slate-300 mt-1 rounded-md' key={index}>
                 {party.character}
               </li>
@@ -43,7 +43,7 @@ export default async function PartyListDetail({ scheduleId }: Ownprops) {
         <div>
           <div className='bg-violet-600 rounded-md font-bold text-white'>2번파티</div>
           <ul className='gap-1'>
-            {data?.characters.party0.map((party: ScheduleData, index: number) => (
+            {data.characters.party0.map((party: ScheduleData, index: number) => (
               <li className='bg-slate-300 mt-1 rounded-md' key={index}>
                 {party.character}
               </li>
