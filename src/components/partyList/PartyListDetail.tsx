0a880b3a5c725cd6beff250f7409f6e87a155ebe
kongchip <myunghwan0321@gmail.com>
2024-03-14 22:41:29 +0900

    🔥 raidList 관련 플래그 삭제 및 PartyDetail 컴포넌트에서 CompactRaidCard 컴포넌트 사용

--- src/components/partyList/PartyListDetail.tsx
+++ src/components/partyList/PartyListDetail.tsx
@@ -1,6 +1,10 @@
 import { getScheduleData } from '@/api/firebase/getScheduleData/getScheduleData'
 import Link from 'next/link'
 
+import { extractBossRank } from '@/utils/extractBossRank'
+import { extractCapacity } from '@/utils/extractCapacity'
+import CompactRaidCard from '../dashboard/CompactRaidCard'
+
 interface Ownprops {
   scheduleId: string
 }
@@ -26,29 +30,43 @@ export default async function PartyListDetail({ scheduleId }: Ownprops) {
   const data = await getScheduleData(scheduleId)
 
   if (!data) return
+  const { boss, rank } = extractBossRank(data.raidName)
+  const capacity = extractCapacity(data.raidType)
+  const participants = data.participants.length
 
   return (
-    <div className='grid grid-cols-2 gap-4 pt-2 px-3 text-center'>
-      <div>
-        <div className='bg-lime-400 rounded-md font-bold text-white '>1번파티</div>
-        <ul className='gap-1'>
-          {data.characters.party0.map((party: ScheduleData, index: number) => (
-            <li className='bg-slate-300 mt-1 rounded-md' key={index}>
-              {party.character}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div>
-        <div className='bg-violet-600 rounded-md font-bold text-white'>2번파티</div>
-        <ul className='gap-1'>
-          {data.characters.party0.map((party: ScheduleData, index: number) => (
-            <li className='bg-slate-300 mt-1 rounded-md' key={index}>
-              {party.character}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    <li>
+      <Link href={`/${data.channel}/schedule/${scheduleId}`}>
+        <CompactRaidCard
+          boss={boss}
+          rank={rank}
+          leader={data.raidLeader.character}
+          date={data.raidDate}
+          headCount={`${participants} / ${capacity}`}
+        />
+        <div className='grid grid-cols-2 gap-4 pt-2 px-3 text-center'>
+          <div>
+            <div className='bg-lime-400 rounded-md font-bold text-white '>1번파티</div>
+            <ul className='gap-1'>
+              {data.characters.party0.map((party: ScheduleData, index: number) => (
+                <li className='bg-slate-300 mt-1 rounded-md' key={index}>
+                  {party.character}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <div className='bg-violet-600 rounded-md font-bold text-white'>2번파티</div>
+            <ul className='gap-1'>
+              {data.characters.party0.map((party: ScheduleData, index: number) => (
+                <li className='bg-slate-300 mt-1 rounded-md' key={index}>
+                  {party.character}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </Link>
+    </li>
   )
 }
