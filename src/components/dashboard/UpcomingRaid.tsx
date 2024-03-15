ced1656c92695681d1b542cbb6a05be596b43568
seungmin2ee <tmdals2815@gmail.com>
2024-03-16 03:46:02 +0900

    💄 다가오는레이드 스타일 추가, 수정

--- src/components/dashboard/UpcomingRaid.tsx
+++ src/components/dashboard/UpcomingRaid.tsx
@@ -11,9 +11,9 @@ export default async function UpcomingRaid({ channelId }: Ownprops) {
 
   if (!data) return
   return (
-    <div className='card min-w-[340px] basis-1/3 bg-secondary-accent/50 dark:bg-primary-accent/80'>
-      <div className='pb-4 text-xl'>다가오는 레이드</div>
-      <ul className='flex flex-col gap-3'>
+    <div className='relative card min-w-[340px] basis-1/3 p-0 bg-[#fee2e5] dark:bg-[#0080b7] overflow-y-auto'>
+      <div className='p-5 pb-4 text-xl bg-inherit sticky top-0 left-0 z-20'>다가오는 레이드</div>
+      <ul className='flex flex-col gap-3 px-5 pb-5'>
         {data.schedules.map((scheduleId: string) => (
           <RaidList key={scheduleId} scheduleId={scheduleId} />
         ))}
