4c736f73f7b7725d138074606a8cfc0d628d807f
seungmin2ee <tmdals2815@gmail.com>
2024-03-17 23:46:08 +0900

    💄 overflow 관련 스타일 수정

--- src/components/dashboard/UpcomingRaid.tsx
+++ src/components/dashboard/UpcomingRaid.tsx
@@ -11,13 +11,15 @@ export default async function UpcomingRaid({ channelId }: Ownprops) {
 
   if (!data) return
   return (
-    <div className='relative card min-w-[340px] basis-1/3 p-0 bg-[#fee2e5] dark:bg-[#0080b7] overflow-y-auto'>
-      <div className='p-5 pb-4 text-xl bg-inherit sticky top-0 left-0 z-20'>다가오는 레이드</div>
-      <ul className='flex flex-col gap-3 px-5 pb-5'>
-        {data.schedules.map((scheduleId: string) => (
-          <RaidList key={scheduleId} scheduleId={scheduleId} />
-        ))}
-      </ul>
+    <div className='card flex flex-col min-w-[340px] basis-1/3 p-0 overflow-hidden'>
+      <div className='relative flex flex-col bg-[#fee2e5] dark:bg-[#0080b7] overflow-y-auto'>
+        <div className='p-5 pb-4 text-xl bg-inherit sticky top-0 left-0 z-20'>다가오는 레이드</div>
+        <ul className='flex flex-col gap-3 px-5 pb-5'>
+          {data.schedules.map((scheduleId: string) => (
+            <RaidList key={scheduleId} scheduleId={scheduleId} />
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
