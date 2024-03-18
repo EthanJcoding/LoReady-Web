bf526c8b9f461d64532a3539c74c5e3fb8398b5c
ethanJcoding <ethan.95j@gmail.com>
2024-03-18 19:11:44 +0900

    ✅ next-auth route 추가

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,25 @@
+import { JWT } from 'next-auth/jwt'
+import NextAuth from 'next-auth/next'
+import Discord from 'next-auth/providers/discord'
+import { Session } from 'next-auth'
+
+const authOptions = {
+  providers: [
+    Discord({
+      clientId: process.env.NEXT_PUBLIC_DISCORD_ID ?? '',
+      clientSecret: process.env.NEXT_PUBLIC_DISCORD_SECRET ?? ''
+    })
+  ],
+  callbacks: {
+    async session({ session, token }: { session: Session; token: JWT }) {
+      if (session?.user) {
+        session.user.id = token.sub
+      }
+      return session
+    }
+  }
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
