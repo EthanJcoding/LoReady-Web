7837b110417e63a2798a0646179e6695d06bafe1
seungmin2ee <tmdals2815@gmail.com>
2024-03-20 22:03:07 +0900

    🔥 불필요한 코드 삭제

--- src/app/[channelId]/dashboard/page.tsx
+++ src/app/[channelId]/dashboard/page.tsx
@@ -1,4 +1,3 @@
-import { getChannelData } from '@/api/firebase'
 import AdditionalConts from '@/components/dashboard/AdditionalConts'
 import UpcomingRaid from '@/components/dashboard/UpcomingRaid'
 
@@ -11,11 +10,7 @@ interface Ownprops {
 export default async function Dashboard({ params: { channelId } }: Ownprops) {
   //FIXME: 작업 완료 후 channelId 할당 삭제하기
   channelId = '1050686760373469234'
-  const data = await getChannelData(channelId)
 
-  console.log(data)
-
-  if (!data) return
   return (
     <div className='flex-1 flex gap-5 overflow-hidden'>
       <UpcomingRaid channelId={channelId} />
