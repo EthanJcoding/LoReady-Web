f6161bee4b061b09b1d76c4249328c500b5d71c8
seungmin2ee <tmdals2815@gmail.com>
2024-03-21 19:28:28 +0900

    ✨ '이번주 도전 가디언 토벌' 컨텐츠 추가

--- src/components/dashboard/AdditionalConts.tsx
+++ src/components/dashboard/AdditionalConts.tsx
@@ -1,9 +1,11 @@
 import AbyssContent from './AbyssContent'
+import GuaridanContent from './GuaridanContent'
 
 export default function AdditionalConts() {
   return (
     <div className='flex-1 grid grid-cols-2 grid-rows-2 gap-5'>
       <AbyssContent />
+      <GuaridanContent />
     </div>
   )
 }
