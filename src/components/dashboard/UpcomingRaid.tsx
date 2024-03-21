f72b5a5a109fe3f362e1aaf61404591dc270cd83
seungmin2ee <tmdals2815@gmail.com>
2024-03-21 19:29:47 +0900

    💄 타이틀 스타일 수정

--- src/components/dashboard/UpcomingRaid.tsx
+++ src/components/dashboard/UpcomingRaid.tsx
@@ -11,7 +11,9 @@ export default async function UpcomingRaid({ channelId }: Ownprops) {
   return (
     <div className='card flex flex-col min-w-[340px] basis-1/3 p-0 overflow-hidden'>
       <div className='relative flex flex-col h-full bg-[#fee2e5] dark:bg-[#0080b7] overflow-y-auto'>
-        <div className='p-5 pb-4 text-xl bg-inherit sticky top-0 left-0 z-20'>다가오는 레이드</div>
+        <div className='flex justify-between p-5 pb-4 text-xl font-medium bg-inherit sticky top-0 left-0 z-20'>
+          다가오는 레이드
+        </div>
         <ul className='flex flex-col gap-3 px-5 pb-5'>
           {data.schedules.map((scheduleId: string) => (
             <RaidList key={scheduleId} scheduleId={scheduleId} />
