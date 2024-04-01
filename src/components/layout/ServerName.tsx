79333dca20e53fca3837822eb701b7b12fad55d1
seungmin2ee <tmdals2815@gmail.com>
2024-04-01 23:08:41 +0900

    🔥 channelId 할당 삭제

--- src/components/layout/ServerName.tsx
+++ src/components/layout/ServerName.tsx
@@ -7,8 +7,6 @@ interface Ownprops {
 }
 
 export default async function ServerName({ id }: Ownprops) {
-  //FIXME: 접속 url 확인되면 아래 id값 재할당 삭제
-  id = '1050686760373469234'
   const data = await getChannelData(id)
 
   if (!data) return
