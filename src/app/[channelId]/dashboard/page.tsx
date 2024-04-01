40a44564785e0d1164045698f92ebc868ebe8a54
seungmin2ee <tmdals2815@gmail.com>
2024-04-01 23:08:41 +0900

    🔥 channelId 할당 삭제

--- src/app/[channelId]/dashboard/page.tsx
+++ src/app/[channelId]/dashboard/page.tsx
@@ -8,9 +8,6 @@ interface Ownprops {
 }
 
 export default async function Dashboard({ params: { channelId } }: Ownprops) {
-  //FIXME: 작업 완료 후 channelId 할당 삭제하기
-  channelId = '1050686760373469234'
-
   return (
     <div className='flex-1 flex gap-5 overflow-hidden'>
       <UpcomingRaid channelId={channelId} />
