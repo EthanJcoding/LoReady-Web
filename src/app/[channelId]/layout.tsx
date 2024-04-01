d7ad25008abe83daafa0a737c865c88298825cbf
seungmin2ee <tmdals2815@gmail.com>
2024-04-02 02:31:30 +0900

    ✨ 채널 유무, 로그인 여부, 채널내 사용자 유무 분기

--- src/app/[channelId]/layout.tsx
+++ src/app/[channelId]/layout.tsx
@@ -1,7 +1,10 @@
+import { getChannelData } from '@/api/firebase'
 import Navigation from '@/components/layout/Navigation'
 import ServerName from '@/components/layout/ServerName'
 import SideBar from '@/components/layout/sidebar/SideBar'
 import ThemeProvider from '@/components/providers/ThemeProvider'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '../api/auth/[...nextauth]/route'
 
 interface Ownprops {
   children: React.ReactNode
@@ -10,7 +13,20 @@ interface Ownprops {
   }
 }
 
-export default function ChannelLayout({ children, params: { channelId } }: Ownprops) {
+export default async function ChannelLayout({ children, params: { channelId } }: Ownprops) {
+  const channelData = await getChannelData(channelId)
+  const session = await getServerSession(authOptions)
+
+  if (!channelData) return <div>유효하지 않은 서버입니다.</div>
+
+  if (!session) return <div>로그인 해주세요.</div>
+
+  const channelMembers: string[] = channelData.memberIds
+  const userId: string = session.user.id
+  const isInChannel = channelMembers.includes(userId)
+
+  if (!isInChannel) return <div>서버에 참여하고 있지 않아요.</div>
+
   return (
     <ThemeProvider>
       <div className='h-dvh flex bg-light dark:bg-dark dark:text-light'>
