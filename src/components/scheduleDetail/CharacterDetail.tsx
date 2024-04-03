555097d593429917f2e8945117b4127c247ecdb2
ethanJcoding <ethan.95j@gmail.com>
2024-04-03 20:01:59 +0900

    🎨 캐릭터 세부정보 확인하는 컴포넌트 추가

---
+++ src/components/scheduleDetail/CharacterDetail.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function CharacterDetail() {
+  return (
+    <section className='border w-full h-full p-4 rounded-lg shadow-sm'>
+      <div>1</div>
+    </section>
+  )
+}
