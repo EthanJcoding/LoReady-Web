ce77dc84aa96cb9099add10bd5dad876e2b3986f
ethanJcoding <ethan.95j@gmail.com>
2024-04-04 17:11:15 +0900

    💄 캐릭터 디테일 컴포넌트 스타일 변경

--- src/components/scheduleDetail/CharacterDetail.tsx
+++ src/components/scheduleDetail/CharacterDetail.tsx
@@ -2,7 +2,7 @@
 
 export default function CharacterDetail() {
   return (
-    <section className='border w-full h-full p-4 rounded-lg shadow-sm'>
+    <section className='border w-2/3 h-full p-8 rounded-lg shadow-sm space-y-4'>
       <div>1</div>
     </section>
   )
