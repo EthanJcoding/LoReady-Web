79a8ff5807c994c6a3276d5250b15ae4ed5b96d2
seungmin2ee <tmdals2815@gmail.com>
2024-04-05 19:25:49 +0900

    ♻️ 개발편의를 위한 Link 경로 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className='w-full h-full flex flex-col justify-center items-center space-y-10'>
-      <Link href={'/123/dashboard'}>
+      <Link href={'/1050686760373469234/dashboard'}>
         <button className='border p-10 rounded-lg'>대쉬보드 개발 페이지로 이동</button>
       </Link>
-      <Link href={'/123/schedule'}>
+      <Link href={'/1050686760373469234/schedule'}>
         <button className='border p-10 rounded-lg'>스케줄 개발 페이지로 이동</button>
       </Link>
     </div>
