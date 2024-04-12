e01afb6403b80e8c460bc68c5cc3f8fd3b083e54
seungmin2ee <tmdals2815@gmail.com>
2024-04-13 05:48:38 +0900

    ♻️ getChannelSchedule, getUserChannels import 경로 수정

--- src/api/firebase/index.ts
+++ src/api/firebase/index.ts
@@ -1,5 +1,7 @@
 import { getChannelData } from './getChannelData/getChannelData'
 import { getScheduleData } from './getScheduleData/getScheduleData'
 import { getUserData } from './getUserData/getUserData'
+import { getChannelSchedule } from './getChannelSchedule/getChannelSchedule'
+import { getUserChannels } from './getUserChannels/getUserChannels'
 
-export { getChannelData, getScheduleData, getUserData }
+export { getChannelData, getScheduleData, getUserData, getChannelSchedule, getUserChannels }
