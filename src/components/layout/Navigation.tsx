51bddd575b0fdba9a8a4e55f662c3a5e36ea3f05
seungmin2ee <tmdals2815@gmail.com>
2024-04-25 01:53:52 +0900

    💄 네비게이션 font weight 수정

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -10,15 +10,15 @@ export default function Navigation() {
     <nav className='border-b border-secondary-gray/50 dark:border-primary-gray/50'>
       <ul className='flex'>
         <li
-          className={`px-4 py-1 font-bold border-dark dark:border-light ${
-            pathname === `/${channelId}/dashboard` ? 'border-b-2' : ''
+          className={`px-4 py-1 border-dark dark:border-light ${
+            pathname === `/${channelId}/dashboard` ? 'border-b-2 font-bold' : 'font-medium'
           }`}
         >
           <Link href={`/${channelId}/dashboard`}>대시보드</Link>
         </li>
         <li
-          className={`px-4 py-1 font-bold border-dark dark:border-light ${
-            pathname === `/${channelId}/schedule` ? 'border-b-2' : ''
+          className={`px-4 py-1 border-dark dark:border-light ${
+            pathname === `/${channelId}/schedule` ? 'border-b-2 font-bold' : 'font-medium'
           }`}
         >
           <Link href={`/${channelId}/schedule`}>레이드 일정</Link>
