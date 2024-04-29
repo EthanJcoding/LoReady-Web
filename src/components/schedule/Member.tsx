48f0fea48f906aa00b19ac943284ee14549cc984
seungmin2ee <tmdals2815@gmail.com>
2024-04-30 05:12:42 +0900

    ✨ 4인 레이드일 경우 마지막 4칸 비활성화

--- src/components/schedule/Member.tsx
+++ src/components/schedule/Member.tsx
@@ -1,10 +1,15 @@
 interface Ownprops {
   character?: string
+  isActive?: boolean
 }
 
-export default function Member({ character }: Ownprops) {
+export default function Member({ character, isActive = true }: Ownprops) {
   return (
-    <li className='flex justify-center items-center px-2 border-t border-secondary-gray/50 overflow-hidden'>
+    <li
+      className={`flex justify-center items-center px-2 border-t border-secondary-gray/50 overflow-hidden ${
+        isActive ? '' : 'bg-gray-300'
+      }`}
+    >
       <span className='truncate'>{character}</span>
     </li>
   )
--- src/components/schedule/TeamLists.tsx
+++ src/components/schedule/TeamLists.tsx
@@ -3,17 +3,19 @@ import Member from './Member'
 
 interface Ownprops {
   members: Character[]
+  capacity: string
 }
 
-export default function TeamLists({ members }: Ownprops) {
-  //TODO: 4인팀일 경우 아래 4칸 배경색 gray
+export default function TeamLists({ members, capacity }: Ownprops) {
+  const blanks: undefined[] = [...Array(4 - members.length)]
+
   return (
     <ul className='flex-1 grid grid-rows-4'>
       {members.map(member => (
         <Member key={member.userId} character={member.character} />
       ))}
-      {[...Array(4 - members.length)].map((_, idx) => (
-        <Member key={idx} />
+      {blanks.map((_, idx) => (
+        <Member key={idx} isActive={capacity === '4' ? idx < blanks.length - 2 : true} />
       ))}
     </ul>
   )
