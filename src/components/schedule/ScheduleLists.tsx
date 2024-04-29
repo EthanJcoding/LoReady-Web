ad6c44d0245b8592c1466513aa7da27bdfcc364a
seungmin2ee <tmdals2815@gmail.com>
2024-04-30 04:42:00 +0900

    ♻️ 스케줄 데이터 구조에 따라 컴포넌트 props 정의

--- src/components/schedule/ScheduleLists.tsx
+++ src/components/schedule/ScheduleLists.tsx
@@ -1,20 +1,30 @@
 'use client'
 
+import { useEffect, useState } from 'react'
 import ScheduleList from './ScheduleList'
+import { getChannelSchedule } from '@/api/firebase'
+import { ScheduleWithId } from '@/types/schedule'
 
 interface Ownprops {
   channelId: string
 }
 
 export default function ScheduleLists({ channelId }: Ownprops) {
-  //TODO: 무한스크롤
+  const [schedules, setSchedules] = useState<ScheduleWithId[]>([])
+
+  useEffect(() => {
+    getChannelSchedule(channelId)
+      .then(res => {
+        setSchedules(res.data)
+      })
+      .catch(error => console.log(error))
+  }, [])
+
   return (
     <ul className='grid grid-cols-4 gap-5 max-xl:grid-cols-3'>
-      <ScheduleList />
-      <ScheduleList />
-      <ScheduleList />
-      <ScheduleList />
-      <ScheduleList />
+      {schedules.map(schedule => (
+        <ScheduleList key={schedule.id} schedule={schedule} />
+      ))}
     </ul>
   )
 }
