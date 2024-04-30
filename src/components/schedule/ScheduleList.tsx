d8d141a13fd01db0db788009d1e98b5ee9472b94
seungmin2ee <tmdals2815@gmail.com>
2024-04-30 16:56:22 +0900

    ✨ 각 스케줄에 스케줄상세페이지 연결

--- src/components/schedule/ScheduleList.tsx
+++ src/components/schedule/ScheduleList.tsx
@@ -1,11 +1,12 @@
-import { Schedule } from '@/types/schedule'
+import { ScheduleWithId } from '@/types/schedule'
 import CompactRaidCard from '../dashboard/CompactRaidCard'
 import TeamLists from './TeamLists'
 import { extractBossRank } from '@/utils/extractBossRank'
 import { extractCapacity } from '@/utils/extractCapacity'
+import Link from 'next/link'
 
 interface Ownprops {
-  schedule: Schedule
+  schedule: ScheduleWithId
 }
 
 export default function ScheduleList({ schedule }: Ownprops) {
@@ -13,27 +14,27 @@ export default function ScheduleList({ schedule }: Ownprops) {
   const capacity = extractCapacity(schedule.raidType)
   const participant = schedule.participants.length
 
-  console.log(schedule)
-
   return (
-    <li className='h-80 flex flex-col bg-white shadow-md shadow-primary-gray/5 border border-secondary-gray/50 rounded-md overflow-hidden'>
-      <CompactRaidCard
-        boss={boss}
-        rank={rank}
-        leader={schedule.raidLeader.character}
-        date={schedule.raidDate}
-        headCount={`${participant} / ${capacity}`}
-      />
-      <div className='h-full grid grid-cols-2 border-t border-inherit text-dark'>
-        <div className='flex flex-col border-r border-inherit bg-pink-50'>
-          <span className='px-2 py-1 text-center font-semibold bg-pink-100'>Team 1</span>
-          <TeamLists members={schedule.parties.party1} capacity={capacity} />
+    <li className='h-80 shadow-md shadow-primary-gray/5 border border-secondary-gray/50 rounded-md overflow-hidden'>
+      <Link href={`/${schedule.channel}/schedule/${schedule.id}`} className='h-full flex flex-col'>
+        <CompactRaidCard
+          boss={boss}
+          rank={rank}
+          leader={schedule.raidLeader.character}
+          date={schedule.raidDate}
+          headCount={`${participant} / ${capacity}`}
+        />
+        <div className='h-full grid grid-cols-2 border-t border-inherit text-dark'>
+          <div className='flex flex-col border-r border-inherit bg-pink-50'>
+            <span className='px-2 py-1 text-center font-semibold bg-pink-100'>Team 1</span>
+            <TeamLists members={schedule.parties.party1} capacity={capacity} />
+          </div>
+          <div className='flex flex-col bg-blue-50'>
+            <span className='px-2 py-1 text-center font-semibold bg-blue-100'>Team 2</span>
+            <TeamLists members={schedule.parties.party2} capacity={capacity} />
+          </div>
         </div>
-        <div className='flex flex-col bg-blue-50'>
-          <span className='px-2 py-1 text-center font-semibold bg-blue-100'>Team 2</span>
-          <TeamLists members={schedule.parties.party2} capacity={capacity} />
-        </div>
-      </div>
+      </Link>
     </li>
   )
 }
