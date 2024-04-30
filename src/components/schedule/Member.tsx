5359514c372c3634bd1a10502bd96f3ec75c8818
seungmin2ee <tmdals2815@gmail.com>
2024-04-30 16:56:22 +0900

    ✨ 각 스케줄에 스케줄상세페이지 연결

--- src/components/schedule/Member.tsx
+++ src/components/schedule/Member.tsx
@@ -7,7 +7,7 @@ export default function Member({ character, isActive = true }: Ownprops) {
   return (
     <li
       className={`flex justify-center items-center px-2 border-t border-secondary-gray/50 overflow-hidden ${
-        isActive ? '' : 'bg-gray-300'
+        isActive ? '' : 'bg-neutral-300'
       }`}
     >
       <span className='truncate'>{character}</span>
