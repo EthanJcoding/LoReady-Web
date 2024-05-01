09ab418e12eb54eb3d58b03caf90030cf1ea7024
ethanJcoding <ethan.95j@gmail.com>
2024-05-01 16:16:28 +0900

    💄 프로필 컴포넌트 위치 조정

--- src/components/auth/Profile.tsx
+++ src/components/auth/Profile.tsx
@@ -7,7 +7,7 @@ export default function Profile() {
   const { data: session } = useSession()
 
   return (
-    <div className='flex gap-4 w-full justify-end'>
+    <div className='flex gap-4 w-full justify-center'>
       <Image
         src={session?.user.image}
         width={10}
