8971be13262bc94a21091fe8f06f6a5d24075344
ethanJcoding <ethan.95j@gmail.com>
2024-05-01 16:15:35 +0900

    📱 반응형을 위한 메뉴 버튼 및 컴포넌트 제작

---
+++ src/components/layout/menu/MenuContent.tsx
@@ -0,0 +1,47 @@
+import ThemeButton from '../sidebar/ThemeButton'
+import ServerLists from '../sidebar/ServerLists'
+import { MdEmail } from 'react-icons/md'
+import { BsHash } from 'react-icons/bs'
+import AuthButton from '@/components/auth/AuthButton'
+
+export default function MenuContent() {
+  return (
+    <aside className='z-40 fixed top-0 2xl:left-0 h-screen w-72 bg-white border-r border-secondary-gray/50 shadow-2xl peer-focus:left-0 -left-96 peer:transition ease-out delay-150 duration-200 flex flex-col dark:bg-neutral-900 dark:border-primary-gray/50'>
+      <header className='flex items-center p-7'>
+        <div className='flex flex-col w-full space-y-8'>
+          <div className='flex w-full justify-between'>
+            <a className='text-2xl text-blue-700' href='/'>
+              LoReady
+            </a>
+            <div className='flex items-center'>
+              <ThemeButton />
+            </div>
+          </div>
+          <AuthButton />
+        </div>
+      </header>
+      <div className='flex-1 flex flex-col justify-center px-7 py-14 overflow-hidden'>
+        <div className='flex items-center text-sm pb-3'>
+          <span>
+            <BsHash size={18} />
+          </span>
+          참여중인 서버
+        </div>
+        <div className='flex-1 overflow-y-auto'>
+          <ServerLists />
+        </div>
+      </div>
+      <footer className='flex flex-col gap-5 px-7 py-5 border-t border-inherit'>
+        <ul className='flex gap-5 text-base text-inherit'>
+          <li className='flex items-center gap-1'>
+            <span>
+              <MdEmail size='20' />
+            </span>
+            Contact us
+          </li>
+        </ul>
+        <div className='text-xs text-primary-gray'>&copy; 2024. LoReady. All rights reserved.</div>
+      </footer>
+    </aside>
+  )
+}
