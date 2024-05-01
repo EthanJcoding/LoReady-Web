077e1c2831bebe42521defc3bd817c56c7ce20fc
ethanJcoding <ethan.95j@gmail.com>
2024-05-01 16:14:43 +0900

    📱사이드바 반응형 hidden 속성 추가

--- src/components/layout/sidebar/SideBar.tsx
+++ src/components/layout/sidebar/SideBar.tsx
@@ -2,18 +2,22 @@ import ServerLists from './ServerLists'
 import ThemeButton from './ThemeButton'
 import { MdEmail } from 'react-icons/md'
 import { BsHash } from 'react-icons/bs'
+import AuthButton from '@/components/auth/AuthButton'
 
 export default function SideBar() {
   return (
-    <aside className='w-72 flex flex-col bg-white border-r border-secondary-gray/50 dark:bg-neutral-900 dark:border-primary-gray/50'>
+    <aside className='w-72 2xl:flex 2xl:flex-col hidden bg-white border-r border-secondary-gray/50 dark:bg-neutral-900 dark:border-primary-gray/50'>
       <header className='flex items-center p-7'>
-        <div className='flex w-full justify-between'>
-          <a className='text-2xl text-blue-700' href='/'>
-            LoReady
-          </a>
-          <div className='flex items-center'>
-            <ThemeButton />
+        <div className='flex flex-col w-full space-y-8'>
+          <div className='flex w-full justify-between'>
+            <a className='text-2xl text-blue-700' href='/'>
+              LoReady
+            </a>
+            <div className='flex items-center'>
+              <ThemeButton />
+            </div>
           </div>
+          <AuthButton />
         </div>
       </header>
       <div className='flex-1 flex flex-col justify-center px-7 py-14 overflow-hidden'>
