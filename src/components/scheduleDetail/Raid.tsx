5bb050370902535b236acf18956ca9380a598298
ethanJcoding <ethan.95j@gmail.com>
2024-05-06 15:35:30 +0900

    ✏️ Raid 컴포넌트 클래스명 타이포 수정

--- src/components/scheduleDetail/Raid.tsx
+++ src/components/scheduleDetail/Raid.tsx
@@ -14,7 +14,7 @@ export default function Raid({ parties, raidType }: Ownprops) {
   const [selectedCharacter, setSelectedCharacter] = useState(parties.party1[0])
 
   return (
-    <div className='flex-1 flex gap-4 h-1/2'>
+    <div className='flex-1 flex gap-4 h-1/2 '>
       <TeamAllocator parties={parties} setSelectedCharacter={setSelectedCharacter} raidType={raidType} />
       <CharacterDetail selectedCharacter={selectedCharacter} />
     </div>
