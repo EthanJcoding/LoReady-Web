a1dfa351fca221740dd56b64924f8bc00028b8ff
ethanJcoding <ethan.95j@gmail.com>
2024-05-06 15:34:55 +0900

    💄 프로필 요약 반응형 추가

--- src/components/scheduleDetail/ProfileSummary.tsx
+++ src/components/scheduleDetail/ProfileSummary.tsx
@@ -8,20 +8,20 @@ export default function ProfileSummary({ ArmoryProfile }: Ownprops) {
   return (
     <div className='flex flex-col space-y-2'>
       <div className='flex w-full space-x-2 items-center'>
-        <div className='border rounded p-1 text-sm font-semibold'>서버</div>
-        <div className='text-lg font-semibold'>{ArmoryProfile.ServerName}</div>
+        <div className='border rounded px-1 text-sm font-semibold truncate'>서버</div>
+        <div className='2xl:text-lg text-sm font-semibold truncate'>{ArmoryProfile.ServerName}</div>
       </div>
       <div className='flex w-full space-x-2 items-center'>
-        <div className='border rounded p-1 text-sm font-semibold'>전투</div>
-        <div className='text-lg font-semibold'>Lv. {ArmoryProfile.CharacterLevel}</div>
+        <div className='border rounded px-1 text-sm font-semibold truncate'>전투</div>
+        <div className='2xl:text-lg text-sm font-semibold truncate'>Lv. {ArmoryProfile.CharacterLevel}</div>
       </div>
       <div className='flex w-full space-x-2 items-center'>
-        <div className='border rounded p-1 text-sm font-semibold'>원정대</div>
-        <div className='text-lg font-semibold'>Lv. {ArmoryProfile.ExpeditionLevel}</div>
+        <div className='border rounded px-1 text-sm font-semibold truncate'>원정대</div>
+        <div className='2xl:text-lg text-sm font-semibold truncate'>Lv. {ArmoryProfile.ExpeditionLevel}</div>
       </div>
       <div className='flex w-full space-x-2 items-center'>
-        <div className='border rounded p-1 text-sm font-semibold '>아이템</div>
-        <div className='text-lg font-semibold'>Lv. {ArmoryProfile.ItemAvgLevel}</div>
+        <div className='border rounded px-1 text-sm font-semibold truncate '>아이템</div>
+        <div className='2xl:text-lg text-sm font-semibold truncate'>Lv. {ArmoryProfile.ItemAvgLevel}</div>
       </div>
     </div>
   )
