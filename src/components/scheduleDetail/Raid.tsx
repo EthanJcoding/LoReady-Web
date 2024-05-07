dcd8cf2b96c8f8080f8f41c8038785c0af07fc20
ethanJcoding <ethan.95j@gmail.com>
2024-05-07 19:37:29 +0900

    ✨ 파티 저장하기 기능구현

--- src/components/scheduleDetail/Raid.tsx
+++ src/components/scheduleDetail/Raid.tsx
@@ -11,10 +11,12 @@ interface Ownprops {
 }
 
 export default function Raid({ parties, raidType }: Ownprops) {
-  const [selectedCharacter, setSelectedCharacter] = useState(parties.party1[0])
+  const defaultCharacter = parties.party1.length === 0 ? parties.party2[0] : parties.party1[0]
+
+  const [selectedCharacter, setSelectedCharacter] = useState(defaultCharacter)
 
   return (
-    <div className='flex-1 flex gap-4 h-1/2 '>
+    <div className='flex-1 flex flex-col gap-4 sm:flex-row overflow-scroll'>
       <TeamAllocator parties={parties} setSelectedCharacter={setSelectedCharacter} raidType={raidType} />
       <CharacterDetail selectedCharacter={selectedCharacter} />
     </div>
