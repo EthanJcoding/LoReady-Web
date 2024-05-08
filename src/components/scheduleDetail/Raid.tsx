5e04616f804678a72906b6a6af611cef029b4312
ethanJcoding <ethan.95j@gmail.com>
2024-05-08 18:00:10 +0900

    💄 어떤 캐릭터를 select 했는지 보여주는 스타일 추가

--- src/components/scheduleDetail/Raid.tsx
+++ src/components/scheduleDetail/Raid.tsx
@@ -17,7 +17,12 @@ export default function Raid({ parties, raidType }: Ownprops) {
 
   return (
     <div className='flex-1 flex flex-col gap-4 sm:flex-row overflow-scroll'>
-      <TeamAllocator parties={parties} setSelectedCharacter={setSelectedCharacter} raidType={raidType} />
+      <TeamAllocator
+        parties={parties}
+        setSelectedCharacter={setSelectedCharacter}
+        selectedCharacter={selectedCharacter}
+        raidType={raidType}
+      />
       <CharacterDetail selectedCharacter={selectedCharacter} />
     </div>
   )
