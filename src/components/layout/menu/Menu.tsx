2e8e0fa160e8dbaf3b9acd855fb9f9fb8c12714f
ethanJcoding <ethan.95j@gmail.com>
2024-05-15 19:32:08 +0900

    💄 햄버거 매뉴 opacity 조절

--- src/components/layout/menu/Menu.tsx
+++ src/components/layout/menu/Menu.tsx
@@ -6,7 +6,7 @@ export default function Menu() {
     <div className='2xl:hidden flex'>
       <MenuTrigger />
       <MenuContent />
-      <div className='z-30 hidden peer-focus:flex fixed top-0 left-0 w-screen h-screen bg-gray-900 bg-opacity-30 opacity-0 peer-focus:opacity-100 peer:transition duration-200'></div>
+      <div className='z-30 hidden peer-focus:flex fixed top-0 left-0 w-screen h-screen bg-gray-900/30 opacity-0 peer-focus:opacity-100 peer:transition duration-200'></div>
     </div>
   )
 }
