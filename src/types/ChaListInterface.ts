91acaf2082db06cb74d294794c9b32dfa22ee200
ethanJcoding <ethan.95j@gmail.com>
2024-05-15 19:37:12 +0900

    🏷️ 캐릭터 리스트의 타입 인터페이스 생성

---
+++ src/types/ChaListInterface.ts
@@ -0,0 +1,8 @@
+export interface ChaListInterface {
+  CharacterClassName: string
+  CharacterLevel: number
+  CharacterName: string
+  ItemAvgLevel: string
+  ItemMaxLevel: string
+  ServerName: string
+}
