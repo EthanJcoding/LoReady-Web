7f23ce72a4110e1d6d7f98c5941fe70ab6267645
seungmin2ee <tmdals2815@gmail.com>
2024-05-15 19:29:33 +0900

    📱 대시보드 페이지 반응형 적용

--- src/components/dashboard/AdditionalConts.tsx
+++ src/components/dashboard/AdditionalConts.tsx
@@ -3,7 +3,7 @@ import GuaridanContent from './GuaridanContent'
 
 export default function AdditionalConts() {
   return (
-    <div className='flex-1 grid grid-cols-2 grid-rows-2 gap-5'>
+    <div className='flex-1 grid grid-cols-2 grid-rows-2 gap-5 max-lg:grid-cols-1 max-sm:hidden'>
       <AbyssContent />
       <GuaridanContent />
     </div>
