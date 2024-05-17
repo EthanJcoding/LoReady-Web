a7f04cf7e60a5f73b1d476085b75b34014e9ab3c
ethanJcoding <ethan.95j@gmail.com>
2024-05-17 17:42:55 +0900

    ✏️ 타입명 변경

--- src/api/firebase/editPartyCharacter/editPartyCharacter.ts
+++ src/api/firebase/editPartyCharacter/editPartyCharacter.ts
@@ -1,6 +1,6 @@
 import { doc, getDoc, updateDoc, serverTimestamp } from 'firebase/firestore'
 import { firestore } from '../config'
-import { Character } from '@/types/schedule'
+import { Character } from '@/types/Schedule'
 
 interface characterData {
   userId: string
--- src/components/dashboard/UpcomingRaid.tsx
+++ src/components/dashboard/UpcomingRaid.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useRef, useState } from 'react'
 import RaidList from './RaidList'
 import { DocumentData } from 'firebase/firestore'
-import { ScheduleWithId } from '@/types/schedule'
+import { ScheduleWithId } from '@/types/Schedule'
 import { getChannelSchedule } from '@/api/firebase'
 
 interface Ownprops {
