153f0b69fdf4e697216e51e7695255b7fffd329c
seungmin2ee <tmdals2815@gmail.com>
2024-05-20 00:11:40 +0900

    📱  다크모드 버튼 사이즈 수정

--- src/components/layout/sidebar/ThemeButton.tsx
+++ src/components/layout/sidebar/ThemeButton.tsx
@@ -12,10 +12,14 @@ export default function ThemeButton() {
 
   return (
     <button
-      className='p-1 border border-secondary-gray/50 rounded-md bg-transparent text-primary-gray hover:bg-light dark:border-primary-gray/50 dark:text-light hover:dark:bg-dark'
+      className='w-9 h-9 p-1 border border-secondary-gray/50 rounded-md bg-transparent text-primary-gray hover:bg-light dark:border-primary-gray/50 dark:text-light hover:dark:bg-dark'
       onClick={handleClick}
     >
-      {currentTheme === 'dark' ? <MdOutlineDarkMode size='26' /> : <MdOutlineLightMode size='26' />}
+      {currentTheme === 'dark' ? (
+        <MdOutlineDarkMode className='w-full h-full' />
+      ) : (
+        <MdOutlineLightMode className='w-full h-full' />
+      )}
     </button>
   )
 }
