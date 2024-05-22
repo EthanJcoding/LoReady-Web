170526d2d20e03563e8c409637eb3eb16d4e38e6
ethanJcoding <ethan.95j@gmail.com>
2024-05-22 17:43:47 +0900

    🚚 파일 이름 형식 통일

--- src/components/scheduleDetail/TeamAllocator/Popover.tsx
+++ src/components/scheduleDetail/TeamAllocator/Popover.tsx
@@ -1,12 +1,11 @@
 import { Character } from '@/types/raid'
 import { getCharacterList } from '@/api/lostark/getCharacterList'
-import { ChaListInterface } from '@/types/Equipments/characterList'
+import { ChaListInterface } from '@/types/characterList'
 import { FaSort, FaCheck } from 'react-icons/fa'
 import { addUserToRaid, deleteUserFromRaid, editPartyCharacter, getUserData } from '@/api/firebase'
-import { useParams } from 'next/navigation'
 import { useSession } from 'next-auth/react'
 import { useState, useEffect } from 'react'
-import { User } from '@/types/User'
+import { User } from '@/types/users'
 
 interface OwnProps {
   targetCharacter: string
