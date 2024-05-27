057011800e4030904d0f485fb0069615f8bad7c3
ethanJcoding <ethan.95j@gmail.com>
2024-05-27 23:48:59 +0900

    💄 스케줄 삭제 버튼 추가

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,13 +1,17 @@
 'use client'
 import Link from 'next/link'
 import { useParams, usePathname } from 'next/navigation'
+import ScheduleDeleteBtn from '../scheduleDetail/ScheduleDelete/ScheduleDeleteBtn'
+import { useState } from 'react'
+import DeleteModal from '../scheduleDetail/ScheduleDelete/DeleteModal'
 
 export default function Navigation() {
   const { channelId } = useParams()
   const pathname = usePathname()
+  const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <nav className='border-b border-secondary-gray/50 dark:border-primary-gray/50'>
+    <nav className='border-b border-secondary-gray/50 dark:border-primary-gray/50 flex items-center justify-between'>
       <ul className='flex'>
         <li
           className={`px-4 py-1 border-dark dark:border-light ${
@@ -17,13 +21,15 @@ export default function Navigation() {
           <Link href={`/${channelId}/dashboard`}>대시보드</Link>
         </li>
         <li
-          className={`px-4 py-1 border-dark dark:border-light ${
+          className={`px-4 py-1 border-dark dark:border-light  ${
             pathname.includes('schedule') ? 'border-b-2 font-bold' : 'font-medium'
           }`}
         >
           <Link href={`/${channelId}/schedule`}>레이드 일정</Link>
         </li>
       </ul>
+      <ScheduleDeleteBtn setIsOpen={setIsOpen} />
+      {isOpen && <DeleteModal setIsOpen={setIsOpen} />}
     </nav>
   )
 }
---
+++ src/components/scheduleDetail/ScheduleDelete/DeleteModal.tsx
@@ -0,0 +1,25 @@
+interface Ownprops {
+  setIsOpen: (arg0: boolean) => void
+}
+
+export default function DeleteModal({ setIsOpen }: Ownprops) {
+  return (
+    <>
+      <div className='absolute z-40 w-screen h-full bg-gray-900/30 top-0 left-0' onClick={() => setIsOpen(false)} />
+      <div className='z-50 border fixed top-1/2 left-1/2 w-[15rem] bg-light dark:bg-dark -translate-x-1/2 -translate-y-1/2 shadow-xl rounded flex flex-col p-4 space-y-4'>
+        <h1 className='text-center'>정말 삭제하시겠습니까?</h1>
+        <div className='flex w-full justify-center gap-4'>
+          <button
+            onClick={() => setIsOpen(false)}
+            className='truncate px-2 py-1 transition rounded border hover:bg-primary-gray/50 text-sm font-medium '
+          >
+            취소
+          </button>
+          <button className='truncate px-2 py-1 transition rounded border bg-secondary-accent hover:bg-secondary-accent/80 text-sm font-medium text-dark'>
+            삭제하기
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}
