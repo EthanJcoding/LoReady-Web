2d715ec4ecae8ca82ded8dfa3905ccb4edd6d60d
seungmin2ee <tmdals2815@gmail.com>
2024-06-01 02:30:37 +0900

    ✨ toast 옵션 추가

--- src/hooks/useToast.ts
+++ src/hooks/useToast.ts
@@ -1,21 +1,28 @@
 import { useToastStore } from '@/stores/toast'
+import { ToastOption } from '@/types/toast'
 import { useEffect, useRef } from 'react'
 
 export const useToast = () => {
-  const { setIsShow, setMessage, setToastElKey } = useToastStore(state => state)
+  const { setIsShow, setMessage, setToastType, setToastElKey } = useToastStore(state => state)
   const timer = useRef<NodeJS.Timeout | null>(null)
 
-  const toast = (message: string) => {
+  const toast = (message: string, option?: ToastOption) => {
+    const toastOption: ToastOption = {
+      type: option?.type || 'success',
+      duraition: option?.duraition || 3000
+    }
+
     if (timer.current) clearTimeout(timer.current)
 
     setIsShow(true)
     setMessage(message)
+    setToastType(toastOption.type)
     setToastElKey()
 
     timer.current = setTimeout(() => {
       setIsShow(false)
       timer.current = null
-    }, 3000)
+    }, toastOption.duraition)
   }
 
   useEffect(() => {
