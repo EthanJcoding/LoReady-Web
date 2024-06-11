a0767ea4d0f33dc66619e75a5f88cd1906a2af3a
seungmin2ee <tmdals2815@gmail.com>
2024-06-12 00:25:25 +0900

    ♻️ 로그인 후 이전 경로로 리다이렉트 되도록 callbackUrl 추가

--- src/components/auth/AuthButton.tsx
+++ src/components/auth/AuthButton.tsx
@@ -2,19 +2,23 @@
 
 import { signIn } from 'next-auth/react'
 import { useSession } from 'next-auth/react'
+import { useSearchParams } from 'next/navigation'
 import { FaDiscord } from 'react-icons/fa'
 import Profile from './Profile'
 
 export default function AuthButton() {
   const { data: session } = useSession()
+  const searchParams = useSearchParams()
+  const callbackUrl = searchParams.get('redirect_url') || ''
 
   if (session) {
     return <Profile />
   }
+
   return (
     <button
       className='bg-indigo-500 inline-flex items-center justify-center whitespace-nowrap space-x-4 px-4 py-2 h-10 rounded-lg hover:bg-indigo-500/75 transition'
-      onClick={() => signIn('discord')}
+      onClick={() => signIn('discord', { callbackUrl })}
     >
       <FaDiscord color='white' className='w-8 h-8' />
       <div className='text-sm text-white font-semibold'>디스코드로 로그인</div>
