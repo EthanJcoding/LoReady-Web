5c40a655ad7fe5995d6818cb8febefae2715b921
ethanJcoding <ethan.95j@gmail.com>
2024-06-13 12:21:37 +0900

    🚑 getServerSession 해결

--- src/components/landing/header/Header.tsx
+++ src/components/landing/header/Header.tsx
@@ -1,13 +1,11 @@
-'use client'
-
 import Link from 'next/link'
 import Image from 'next/image'
 import ThemeButton from '@/components/layout/sidebar/ThemeButton'
 import AuthButton from '@/components/auth/AuthButton'
-import { useSession } from 'next-auth/react'
 import { getUserData } from '@/api/firebase'
-import { useEffect, useState } from 'react'
 import { User } from '@/types/users'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/utils/authOptions'
 
 function HeaderLink({
   href,
@@ -30,21 +28,38 @@ function HeaderLink({
   )
 }
 
-export default function Header() {
-  const { data: session } = useSession()
-  const [channelId, setChannelId] = useState<string | null>(null)
+export default async function Header() {
+  const session = await getServerSession(authOptions)
 
-  useEffect(() => {
-    const getChannel = async () => {
-      if (session?.user.id) {
-        const userId = session.user.id
-        const { channels } = (await getUserData(userId)) as User
-        setChannelId(channels[0] || null)
-      }
-    }
+  if (session) {
+    const { channels } = (await getUserData(session.user.id)) as User
 
-    getChannel()
-  }, [session?.user.id])
+    return (
+      <header className='flex justify-between w-full'>
+        <div className='flex space-x-4 items-center'>
+          <Link className='w-28 pt-0.5' href='/'>
+            <Image
+              className='w-full h-full'
+              src='/images/logo.svg'
+              alt='로레디 로고'
+              width={100}
+              height={50}
+              priority
+            />
+          </Link>
+          <div className='shrink-0 h-full w-[1px] border-l hidden sm:flex'></div>
+
+          <HeaderLink href={process.env.NEXT_PUBLIC_DOCUMENT_LINK as string}>사용법</HeaderLink>
+          <HeaderLink href={process.env.NEXT_PUBLIC_ADDBOT_LINK as string}>봇 추가하기</HeaderLink>
+          {<HeaderLink href={`/${channels[0]}/dashboard`}>대시보드</HeaderLink>}
+        </div>
+        <div className='flex items-center gap-4'>
+          <ThemeButton />
+          <AuthButton />
+        </div>
+      </header>
+    )
+  }
 
   return (
     <header className='flex justify-between w-full'>
@@ -56,11 +71,6 @@ export default function Header() {
 
         <HeaderLink href={process.env.NEXT_PUBLIC_DOCUMENT_LINK as string}>사용법</HeaderLink>
         <HeaderLink href={process.env.NEXT_PUBLIC_ADDBOT_LINK as string}>봇 추가하기</HeaderLink>
-        {session && (
-          <HeaderLink href={`/${channelId}/dashboard`} isDisabled={!channelId}>
-            대시보드
-          </HeaderLink>
-        )}
       </div>
       <div className='flex items-center gap-4'>
         <ThemeButton />
