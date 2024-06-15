1646503b3f223667366b73d657117cad7a7d5730
seungmin2ee <tmdals2815@gmail.com>
2024-06-16 00:56:32 +0900

    ♻️  미들웨어 config 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,15 @@ export function middleware(request: NextRequest) {
   const requestHeaders = new Headers(request.headers)
   requestHeaders.set('x-pathname', request.nextUrl.pathname)
 
+  console.log('📍Middleware x-pathname', requestHeaders.get('x-pathname'))
+
   return NextResponse.next({
     request: {
       headers: requestHeaders
     }
   })
+}
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|images|$|login).*)']
 }
