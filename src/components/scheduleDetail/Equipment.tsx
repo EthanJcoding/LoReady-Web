7fd5d2afe8b1bc761de37f63aeb03d7ab019337e
ethanJcoding <ethan.95j@gmail.com>
2024-06-22 23:16:30 +0900

    💄 캐릭터 요약 장비 스타일 width 수정

--- src/components/scheduleDetail/Equipment.tsx
+++ src/components/scheduleDetail/Equipment.tsx
@@ -215,7 +215,7 @@ const BraceletAndStoneDetails = ({ equipment }: { equipment: EquipmentInterface 
           <div className='flex flex-wrap gap-2'>
             {bracelet.map((el, idx) => (
               <div key={idx} className='text-xs font-medium border p-0.5 rounded'>
-                {el}
+                <span>{el}</span>
               </div>
             ))}
           </div>
@@ -240,17 +240,17 @@ export default function Equipment({ ArmoryEquipment }: Ownprops) {
 
   return (
     <div className='flex flex-wrap w-full h-full gap-4 justify-between'>
-      <div className='space-y-2'>
+      <div className='space-y-2 w-[16rem]'>
         {equipments.map((equipment, idx) => (
           <EquipmentDetails key={idx} equipment={equipment} />
         ))}
       </div>
-      <div className='space-y-2 md:w-1/2'>
+      <div className='space-y-2 w-[16rem]'>
         {accessories.map((equipment, idx) => (
           <AccessoryDetails key={idx} equipment={equipment} />
         ))}
       </div>
-      <div className='space-y-2'>
+      <div className='space-y-2 w-max'>
         {braceletAndStone.map((equipment, idx) => (
           <BraceletAndStoneDetails key={idx} equipment={equipment} />
         ))}
