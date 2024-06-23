7edbf75bcf8dc33219a63961ccaf32c71072021c
seungmin2ee <tmdals2815@gmail.com>
2024-06-24 05:16:29 +0900

    📱 랜딩페이지 반응형 수정

--- src/components/landing/animation/animation.tsx
+++ src/components/landing/animation/animation.tsx
@@ -4,5 +4,7 @@ import Lottie from 'react-lottie-player'
 import lottieJson from '../../../../public/images/hero1.json'
 
 export default function Animation() {
-  return <Lottie loop animationData={lottieJson} play className='w-full' />
+  return (
+    <Lottie loop animationData={lottieJson} play className='flex-1 min-w-[640px] max-xl:min-w-[520px] max-lg:hidden' />
+  )
 }
--- src/components/landing/hero/Hero.tsx
+++ src/components/landing/hero/Hero.tsx
@@ -4,18 +4,22 @@ import DemoAuthButton from '@/components/auth/DemoAuthButton'
 
 export default function Hero() {
   return (
-    <main className='w-full h-full flex flex-col sm:p-32 p-8'>
-      <section className='flex flex-col sm:flex-row w-full h-full gap-10'>
-        <div className='flex flex-col items-left justify-center h-full w-full'>
-          <h1 className='sm:text-4xl font-bold mb-4 sm:w-max w-full text-xl'>
-            친구들, 길드원들과 <div className='mb-2' /> 레이드 스케줄을 편하게 관리하세요
-          </h1>
-          <p className='sm:text-xl mb-8'>디스코드에서 레이드 스케줄을 만들고 공유하세요 !</p>
-          <div className='flex items-center gap-4 mb-8'>
-            <div className='flex gap-4'>
+    <main className='w-full h-full flex flex-col justify-center items-center'>
+      <section className='max-w-[1600px] flex w-full h-full gap-10 justify-between px-5 py-32 max-sm:px-3 max-lg:py-0'>
+        <div className='flex-1 flex flex-col items-left justify-center'>
+          <div className='mb-8 font-bold text-4xl max-xl:text-3xl max-lg:text-4xl max-sm:text-3xl'>
+            <p className='mb-2'>친구들, 길드원들과</p>
+            <p className='mb-4'>레이드 스케줄을 편하게 관리하세요.</p>
+            <p className='font-normal text-xl max-xl:text-lg max-lg:text-xl'>
+              디스코드에서 레이드 스케줄을 만들고 공유하세요 !
+            </p>
+          </div>
+          <div className='flex items-center gap-4 mb-8 max-sm:gap-3'>
+            <div className='flex gap-4 max-sm:gap-3'>
               <Link
                 className='text-light bg-primary-accent hover:bg-primary-accent/70 font-medium py-2 px-4 rounded transition'
                 href={process.env.NEXT_PUBLIC_DOCUMENT_LINK as string}
+                target='_blank'
               >
                 사용법
               </Link>
