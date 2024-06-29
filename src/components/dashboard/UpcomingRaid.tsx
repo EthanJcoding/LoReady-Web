2bb05a309cdcb8bd2d5f078454139ced6cc3a4bc
seungmin2ee <tmdals2815@gmail.com>
2024-06-29 23:25:41 +0900

    💄 스크롤바 커스텀 디자인 적용

--- src/components/dashboard/UpcomingRaid.tsx
+++ src/components/dashboard/UpcomingRaid.tsx
@@ -46,7 +46,7 @@ export default function UpcomingRaid({ channelId, schedulesData, initialSnapshot
 
   return (
     <div className='card p-0 flex flex-col basis-full md:basis-1/2 xl:basis-1/3 overflow-hidden' ref={rootRef}>
-      <div className='relative flex flex-col h-full bg-[#fee2e5] dark:bg-[#0080b7] overflow-y-auto'>
+      <div className='relative flex flex-col h-full bg-[#fee2e5] dark:bg-[#0080b7] overflow-y-auto custom-scrollbar'>
         <div className='flex justify-between p-4 pb-4 text-xl font-medium bg-inherit sticky top-0 left-0 z-20 -translate-y-[1px] md:p-5'>
           다가오는 레이드
         </div>
