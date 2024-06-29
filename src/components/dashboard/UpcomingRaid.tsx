92be8020c55adb39941be649be9b28978d708dab
seungmin2ee <tmdals2815@gmail.com>
2024-06-29 22:09:27 +0900

    📱 컨텐츠 max-width 추가 및 반응형 수정

--- src/components/dashboard/UpcomingRaid.tsx
+++ src/components/dashboard/UpcomingRaid.tsx
@@ -45,15 +45,12 @@ export default function UpcomingRaid({ channelId, schedulesData, initialSnapshot
   }, [lastSnapshot])
 
   return (
-    <div
-      className='card flex flex-col min-w-[370px] basis-1/3 p-0 overflow-hidden max-sm:flex-1 max-sm:min-w-full'
-      ref={rootRef}
-    >
+    <div className='card p-0 flex flex-col basis-full md:basis-1/2 xl:basis-1/3 overflow-hidden' ref={rootRef}>
       <div className='relative flex flex-col h-full bg-[#fee2e5] dark:bg-[#0080b7] overflow-y-auto'>
-        <div className='flex justify-between p-5 pb-4 text-xl font-medium bg-inherit sticky top-0 left-0 z-20 -translate-y-[1px]'>
+        <div className='flex justify-between p-4 pb-4 text-xl font-medium bg-inherit sticky top-0 left-0 z-20 -translate-y-[1px] md:p-5'>
           다가오는 레이드
         </div>
-        <ul className='flex flex-col gap-3 px-5 pb-5'>
+        <ul className='flex flex-col gap-3 px-4 pb-5 md:px-5'>
           {schedules.length ? (
             schedules.map(schedule => (
               <RaidList
