c78a79ffe45bc8eef58c662fb7985b4245a15205
seungmin2ee <tmdals2815@gmail.com>
2024-07-09 00:49:25 +0900

    ✨ 컴포넌트 클린업시 상태 초기화

--- src/stores/scheduleFilter.ts
+++ src/stores/scheduleFilter.ts
@@ -2,10 +2,10 @@ import { create } from 'zustand'
 
 interface ScheduleFilterStore {
   isShowMySchedule: boolean
-  setIsShowMySchedule: () => void
+  setIsShowMySchedule: (isShowMySchedule: boolean) => void
 }
 
 export const useScheduleFilterStore = create<ScheduleFilterStore>(set => ({
   isShowMySchedule: false,
-  setIsShowMySchedule: () => set(state => ({ isShowMySchedule: !state.isShowMySchedule }))
+  setIsShowMySchedule: isShowMySchedule => set({ isShowMySchedule })
 }))
