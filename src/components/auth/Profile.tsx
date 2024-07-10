b48506a3f0d1198517be1c01fdcfd7dd980589b9
seungmin2ee <tmdals2815@gmail.com>
2024-07-11 03:28:34 +0900

    🔥 로그아웃 버튼 텍스트 분기 삭제

--- src/components/auth/Profile.tsx
+++ src/components/auth/Profile.tsx
@@ -20,9 +20,7 @@ export default function Profile() {
         className='w-full bg-indigo-500 inline-flex items-center justify-center whitespace-nowrap space-x-2 px-4 h-10 rounded-lg hover:bg-indigo-500/90 transition'
         onClick={() => signOut({ callbackUrl: '/' })}
       >
-        <div className='text-sm text-white font-semibold'>
-          {session?.user.id === 'demo' ? '둘러보기 종료' : '로그아웃'}
-        </div>
+        <div className='text-sm text-white font-semibold'>로그아웃</div>
       </button>
     </div>
   )
