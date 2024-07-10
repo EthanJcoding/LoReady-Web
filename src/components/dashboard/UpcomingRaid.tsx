6adcddd7d4fb6598789b39c0c6df566fda0a3a1b
seungmin2ee <tmdals2815@gmail.com>
2024-07-10 19:52:28 +0900

    ♻️ 다가오는레이드 데이터 fetching 및 무한스크롤 리팩토링

--- src/components/dashboard/UpcomingRaid.tsx
+++ src/components/dashboard/UpcomingRaid.tsx
@@ -1,57 +1,35 @@
 'use client'
 
-import { useEffect, useRef, useState } from 'react'
+import { useEffect } from 'react'
 import RaidList from './RaidList'
-import { Schedule, ScheduleWithId } from '@/types/schedule'
-import { getChannelSchedule } from '@/api/firebase'
+import { useFetchSchedules } from '@/hooks/useFetchSchedules'
+import { useInfiniteScroll } from '@/hooks/useInfiniteScroll'
+import { FaSpinner } from 'react-icons/fa'
 
 interface Ownprops {
   channelId: string
-  schedulesData: ScheduleWithId[]
-  initialSnapshotData: Schedule | undefined
 }
 
-export default function UpcomingRaid({ channelId, schedulesData, initialSnapshotData }: Ownprops) {
-  const [schedules, setSchedules] = useState<ScheduleWithId[]>(schedulesData)
-  const [lastSnapshot, setLastSnapshot] = useState<Schedule | undefined>(initialSnapshotData)
-  const [isMore, setIsMore] = useState(true)
-  const targetRef = useRef<HTMLDivElement>(null)
-  const rootRef = useRef<HTMLDivElement>(null)
+export default function UpcomingRaid({ channelId }: Ownprops) {
+  const { schedules, isPending, isMore, lastSnapRef, fetchSchedules } = useFetchSchedules(channelId)
+  const targetRef = useInfiniteScroll(fetchSchedules, isMore, lastSnapRef)
 
   useEffect(() => {
-    let observer: IntersectionObserver
-
-    const handleIntersect = ([entry]: IntersectionObserverEntry[]) => {
-      if (entry.isIntersecting && isMore) {
-        getChannelSchedule(channelId, lastSnapshot)
-          .then(res => {
-            setSchedules(prevSchedules => [...prevSchedules, ...res.data])
-            setLastSnapshot(res.lastSnap)
-            setIsMore(res.lastSnap !== undefined)
-          })
-          .catch(error => console.log(error))
-      }
-    }
-
-    if (targetRef.current && isMore) {
-      observer = new IntersectionObserver(handleIntersect, {
-        root: rootRef.current
-      })
-
-      observer.observe(targetRef.current)
-    }
-
-    return () => observer && observer.disconnect()
-  }, [lastSnapshot])
+    fetchSchedules(true)
+  }, [])
 
   return (
-    <div className='card p-0 flex flex-col basis-full md:basis-1/2 xl:basis-1/3 overflow-hidden' ref={rootRef}>
+    <div className='card p-0 flex flex-col basis-full md:basis-1/2 xl:basis-1/3 overflow-hidden'>
       <div className='relative flex flex-col h-full bg-[#fee2e5] dark:bg-[#0080b7] overflow-y-auto custom-scrollbar'>
         <div className='flex justify-between p-4 pb-4 text-xl font-medium bg-inherit sticky top-0 left-0 z-20 -translate-y-[1px] md:p-5'>
           다가오는 레이드
         </div>
         <ul className='flex flex-col gap-3 px-4 pb-5 md:px-5'>
-          {schedules.length ? (
+          {isPending ? (
+            <li className='flex justify-center pt-10'>
+              <FaSpinner className='animate-spin text-3xl' color='#00a4e8' />
+            </li>
+          ) : schedules.length ? (
             schedules.map(schedule => (
               <RaidList
                 key={schedule.id}
