dc9c0a415118d0fc55c61f29d2ec4d07ebb959d4
ethanJcoding <ethan.95j@gmail.com>
2024-07-10 17:05:13 +0900

    🚑 겁화 작열 보석 배경 색상 추가

--- src/components/scheduleDetail/CharacterDetail/Gem.tsx
+++ src/components/scheduleDetail/CharacterDetail/Gem.tsx
@@ -10,16 +10,26 @@ export default function Gem({ ArmoryGem }: Ownprops) {
     return <div>보석이 없어요!</div>
   }
 
-  // hover 이벤트 추가
+  const getGemGrade = (grade: string) => {
+    switch (grade) {
+      case '고대':
+        return 'bg-gradient-to-br from-[#3d3325] to-[#dcc999]'
+      case '유물':
+        return 'bg-gradient-to-br from-[#341a09] to-[#a24006]'
+      case '전설':
+        return 'bg-gradient-to-br from-[#341a09] to-[#a24006]'
+      case '에스더':
+        return 'bg-gradient-to-br from-[#0c2e2c] to-[#2faba8]'
+      default:
+        return ''
+    }
+  }
 
   return (
     <div className='h-full'>
       <div className='flex justify-between gap-2'>
         {ArmoryGem.Gems.map((gem, idx) => (
-          <div
-            key={idx}
-            className='flex flex-col items-center bg-gradient-to-br from-[#3c2201] to-[#a86200] rounded-lg'
-          >
+          <div key={idx} className={`flex flex-col items-center rounded-lg ${getGemGrade(gem.Grade)}`}>
             <Image src={gem.Icon} width={50} height={50} alt='보석' className='' />
             <div className='bg-primary-accent text-white w-full text-center rounded-b-lg'>{gem.Level}</div>
           </div>
