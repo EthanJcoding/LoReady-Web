d7b6dd351694aa3756627df7c7f62b5d052634c2
ethanJcoding <ethan.95j@gmail.com>
2024-07-15 00:18:55 +0900

    ✨ 유저 검색 페이지 메타데이터 추가

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,6 +1,21 @@
 import Header from '@/components/landing/header/Header'
 import Search from '@/components/search/Search'
 
+interface Ownprops {
+  searchParams: {
+    query: string
+  }
+}
+
+export async function generateMetadata({ searchParams }: Ownprops) {
+  const characterName = searchParams.query
+
+  return {
+    title: `${characterName} - 유저 검색`,
+    description: '로스트아크의 유저 정보를 확인할 수 있습니다.'
+  }
+}
+
 export default function Characters() {
   return (
     <div className='w-full p-7 space-y-4 h-dvh flex flex-col bg-light dark:bg-dark dark:text-light max-sm:p-5'>
