44cb507ce579f143aa3f03444a2a434ea7f60a9c
ethanJcoding <ethan.95j@gmail.com>
2024-07-14 23:54:16 +0900

    🎨 시간 유틸 함수 날짜 계산 기능추가

--- src/utils/customDateString.ts
+++ src/utils/customDateString.ts
@@ -1,5 +1,7 @@
-function customDateString() {
+function customDateString(daysToSubtract: number = 0) {
   const currentDate = new Date()
+  currentDate.setDate(currentDate.getDate() - daysToSubtract)
+
   const year = currentDate.getFullYear()
   const month = String(currentDate.getMonth() + 1).padStart(2, '0')
   const day = String(currentDate.getDate()).padStart(2, '0')
