d13e870b202499605fd5c76982b751582ae667c3
ethanJcoding <ethan.95j@gmail.com>
2024-07-14 23:53:29 +0900

    📱 헤더링크 반응형 수정

--- src/components/landing/header/Gnb.tsx
+++ src/components/landing/header/Gnb.tsx
@@ -14,17 +14,20 @@ export default async function Gnb() {
   }
 
   return (
-    <ul className='gap-4 hidden sm:flex'>
-      <li>
+    <ul className='gap-4 flex'>
+      <li className='sm:flex hidden'>
         <HeaderLink href={process.env.NEXT_PUBLIC_DOCUMENT_LINK as string} target='_blank'>
           사용법
         </HeaderLink>
       </li>
-      <li>
+      <li className='sm:flex hidden'>
         <HeaderLink href={process.env.NEXT_PUBLIC_ADDBOT_LINK as string}>봇 추가하기</HeaderLink>
       </li>
+      <li className='flex'>
+        <HeaderLink href='/characters'>유저 검색</HeaderLink>
+      </li>
       {session && (
-        <li>
+        <li className='flex'>
           <HeaderLink href={dashboardUrl}>대시보드</HeaderLink>
         </li>
       )}
