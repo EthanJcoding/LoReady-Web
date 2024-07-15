8d0fdf9dbd0f66f929cfb8dfe5a09deb6ab9beef
ethanJcoding <ethan.95j@gmail.com>
2024-07-15 17:44:11 +0900

    💄 padding 값 통일화

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -6,8 +6,8 @@ import SearchSiblings from './SearchSiblings'
 
 export default function Search() {
   return (
-    <main className='max-w-[121rem] flex-1 flex flex-col gap-4 sm:flex-row overflow-hidden'>
-      <section className='min-w-[18rem] flex flex-col px-4 space-y-4'>
+    <main className='max-w-[121rem] flex-1 flex flex-col gap-8 sm:flex-row overflow-hidden'>
+      <section className='min-w-[18rem] flex flex-col space-y-4'>
         <SearchInput />
         <SearchSiblings />
       </section>
